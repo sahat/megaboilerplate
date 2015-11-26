5e3eebe3a5235eeb9e9325ff30952445f9717ebc
Sahat Yalkabov <sakhat@gmail.com>
2015-11-26 13:41:40 -0500

    Add carbon ads

--- src/Hero.js
+++ src/Hero.js
@@ -1,17 +1,33 @@
 import React from 'react';
 
 class Banner extends React.Component {
-    render() {
-        return (
-          <div className="hero">
-            <div className="container">
-              <h1>Mega Boilerplate</h1>
-              <p className="lead">An overview of Bootstrap, how to download and use, basic
-              templates and examples, and more.</p>
-            </div>
-          </div>
-        );
-    }
+
+  componentDidMount() {
+    this.loadCarbonAds();
+  }
+
+  loadCarbonAds() {
+    let carbonAdsContainer = this.refs.carbonAds;
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//cdn.carbonads.com/carbon.js?zoneid=1673&serve=C6AILKT&placement=sahatyalkabovcom';
+    script.id = '_carbonads_js';
+    carbonAdsContainer.appendChild(script);
+  }
+
+  render() {
+    return (
+      <div className="hero">
+        <div className="container">
+          <span className="text-center bd-booticon outline">B</span>
+          <h1>Mega Boilerplate</h1>
+          <p className="lead">An overview of Bootstrap, how to download and use, basic
+            templates and examples, and more.</p>
+          <div ref="carbonAds"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Banner;
