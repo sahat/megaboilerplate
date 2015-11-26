38093b2493281e3a8f0a46aa6e8b5935ee9fb3ee
Sahat Yalkabov <sakhat@gmail.com>
2015-11-26 15:21:37 -0500

    Clean up express app module

---
+++ modules/express/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+/** Express Template Engine
+ * @type templateEngineSetup
+ */
+
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
