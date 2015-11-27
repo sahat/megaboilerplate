533510d8931e4ef3ff642d2bb4e8b11a5f49ba66
Sahat Yalkabov <sakhat@gmail.com>
2015-11-26 20:54:28 -0500

    Added prepare, generateFramework and other functions

--- devServer.js
+++ devServer.js
@@ -1,20 +1,27 @@
+'use strict';
+
+var _ = require('lodash');
+var fs = require('fs-extra');
+var shortid = require('shortid');
 var archiver = require('archiver');
 var jsonfile = require('jsonfile');
-
-var fs = require('fs');
+var del = require('del');
+var mkdirp = require('mkdirp');
+var commentParser = require('comment-parser');
+var Promise = require('bluebird');
 var path = require('path');
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 var webpack = require('webpack');
 var config = require('./webpack.config.dev');
+
+var copy = Promise.promisify(fs.copy);
+var readJson = Promise.promisify(fs.readJson);
+var writeJson = Promise.promisify(fs.writeJson);
 
 var app = express();
 var compiler = webpack(config);
 
-console.dir(jsonfile.readFileSync(__dirname + '/modules/express/package.json'))
-//jsonfile.writeFile(file, obj, function (err) {
-//  console.error(err)
-//})
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'assets')));
@@ -27,25 +34,16 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 
+app.post('/download', (req, res) => {
 
-app.post('/download', function(req, res) {
-  if (req.body.platform === 'node') {
+  prepare().then((uid) => {
+    let framework = req.body.framework;
+    let appName = req.body.appName;
+    generateFramework(framework, appName, uid).then(() => {
 
-    switch (req.body.framework) {
-      case 'express':
-        generateExpressFramework(req, res);
-        break;
-      case 'hapi':
-        break;
-      case 'sails':
-        break;
-      default:
-        res.status(400).send('Unsupported Framework');
-    }
+    });
+  });
 
-  } else {
-    res.status(400).send('Unsupported Platform');
-  }
 });
 
 app.get('*', function(req, res) {
@@ -61,31 +59,104 @@ app.listen(3000, 'localhost', function(err) {
   console.log('Listening at http://localhost:3000');
 });
 
-function generateExpressFramework(req, res) {
-  var archive = archiver('zip');
+function generateFramework(framework, appName, uid) {
+  return new Promise((resolve, reject) => {
+    let dest = path.join(__dirname, 'build', uid);
 
-  archive.on('error', function(err) {
-    res.status(500).send(err.message);
+    switch (framework) {
+      case 'express':
+        let src = path.join(__dirname, 'modules', 'express');
+
+        copy(src, dest).then(() => {
+          let packageJson = path.join(dest, 'package.json');
+          readJson(packageJson).then((packageObj) => {
+            packageObj.name = appName;
+            writeJson(packageJson, packageObj, { spaces: 2 }).then(() => {
+              console.log('done');
+            });
+          });
+        });
+        break;
+      case 'hapi':
+        break;
+      case 'sails':
+        break;
+      default:
+        reject('Unsupported Framework');
+    }
   });
+}
 
-  res.on('close', function() {
-    console.log('closing...')
-    console.log('Archive wrote %d bytes', archive.pointer());
-    return res.status(200).send('OK').end();
+function generateTemplateEngine(params) {
+  return new Promise(function(resolve, reject) {
+    switch (params.templateEngine) {
+      case 'jade':
+        if (params.framework === 'express') {
+          fs.readFile(__dirname + '/modules/express/app.js', function (err, data) {
+            if (err) throw err;
+            console.log(data);
+          });
+          // open app.js
+          // parse comments
+          // open template-engine/jade
+          var parsed = commentParser.parse(data);
+          console.log(parsed);
+        }
+        break;
+      case 'handlebars':
+        break;
+      case 'swig':
+        break;
+      case 'none':
+        break;
+      default:
+        reject('Unsupported template engine');
+    }
   });
+}
 
-  res.attachment('megaboilerplate-express.zip');
+function generateZip(req, res) {
+  return new Promise(function(resolve, reject) {
+    var archive = archiver('zip');
 
-  archive.pipe(res);
+    archive.on('error', function(err) {
+      res.status(500).send(err.message);
+    });
 
-  var files = [
-    __dirname + '/modules/express/app.js',
-    __dirname + '/modules/express/package.json'
-  ];
+    res.on('close', function() {
+      console.log('closing...')
+      console.log('Archive wrote %d bytes', archive.pointer());
+      return res.status(200).send('OK').end();
+    });
 
-  for (var i in files) {
-    archive.append(fs.createReadStream(files[i]), { name: path.basename(files[i]) });
-  }
+    res.attachment('megaboilerplate-express.zip');
 
-  archive.finalize();
+    archive.pipe(res);
+
+    var files = [
+      __dirname + '/modules/express/app.js',
+      __dirname + '/modules/express/package.json'
+    ];
+
+    for (var i in files) {
+      archive.append(fs.createReadStream(files[i]), { name: path.basename(files[i]) });
+    }
+
+    archive.finalize();
+  });
+}
+
+function cleanup() {
+  del(['build/**/*']);
+}
+
+function prepare() {
+  return new Promise((resolve, reject) => {
+    let uid = shortid.generate();
+    let location = path.join(__dirname, 'build', uid);
+    mkdirp(location, (err) => {
+      if (err) { reject(err); }
+      else { resolve(uid); }
+    });
+  });
 }
