8fc630fdd3b4013a20c346d154401bbc163a2eab
Sahat Yalkabov <sakhat@gmail.com>
2015-11-27 04:29:06 -0500

    Added github buttons to hero module

--- src/Hero.js
+++ src/Hero.js
@@ -21,8 +21,9 @@ class Banner extends React.Component {
         <div className="container">
           <span className="text-center bd-booticon outline">B</span>
           <h1>Mega Boilerplate</h1>
-          <p className="lead">An overview of Bootstrap, how to download and use, basic
-            templates and examples, and more.</p>
+          <p className="lead">Clean, simple, easy to use, hand-crafted project starters.</p>
+          <iframe src="https://ghbtns.com/github-btn.html?user=twbs&repo=bootstrap&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="160px" height="30px"></iframe>
+          <iframe src="https://ghbtns.com/github-btn.html?user=sahat&type=follow&count=true&size=large" frameBorder="0" scrolling="0" width="220px" height="30px"></iframe>
           <div ref="carbonAds"></div>
         </div>
       </div>
