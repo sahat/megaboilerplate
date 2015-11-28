0bb9e66c084ac891d7515b33bb830521996c5f42
Sahat Yalkabov <sakhat@gmail.com>
2015-11-27 22:09:56 -0500

    Reduced size of github buttons

--- src/Hero.js
+++ src/Hero.js
@@ -22,8 +22,8 @@ class Banner extends React.Component {
           <span className="text-center bd-booticon outline">B</span>
           <h1>Mega Boilerplate</h1>
           <p className="lead">Clean, simple, easy to use, hand-crafted project starters.</p>
-          <iframe src="https://ghbtns.com/github-btn.html?user=twbs&repo=bootstrap&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="160px" height="30px"></iframe>
-          <iframe src="https://ghbtns.com/github-btn.html?user=sahat&type=follow&count=true&size=large" frameBorder="0" scrolling="0" width="220px" height="30px"></iframe>
+          <iframe src="https://ghbtns.com/github-btn.html?user=sahat&repo=satellizer&type=star&count=true" frameBorder="0" scrolling="0" width="100px" height="20px"></iframe>
+          <iframe src="https://ghbtns.com/github-btn.html?user=sahat&type=follow&count=true" frameBorder="0" scrolling="0" width="170px" height="20px"></iframe>
           <div ref="carbonAds"></div>
         </div>
       </div>
