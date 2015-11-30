814a689c7772afeff5eef6c479197160cb8d6556
Sahat Yalkabov <sakhat@gmail.com>
2015-11-29 22:43:38 -0500

    Keep track of packages and package version in one place

---
+++ modules/packages.js
@@ -0,0 +1,15 @@
+export default {
+  mongodb: {
+    mongoose: '^4.2.8'
+  },
+  mysql: {
+    knex: '^0.9.0',
+    bookshelf: '^0.9.1',
+    mysql: '^2.9.0'
+  },
+  postgresql: {
+    knex: '^0.9.0',
+    bookshelf: '^0.9.1',
+    pg: '^4.4.3'
+  }
+};
