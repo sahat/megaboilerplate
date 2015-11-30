afdccc71a46bda22a7af8a5d8992047aff9ae9b8
Sahat Yalkabov <sakhat@gmail.com>
2015-11-30 02:39:04 -0500

    Fixed express-session deprecated param warnings

--- modules/express/app.js
+++ modules/express/app.js
@@ -16,7 +16,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: 'keyboard cat' }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
 // AUTHENTICATION_MIDDLEWARE
 app.use(express.static(path.join(__dirname, 'public')));
 
