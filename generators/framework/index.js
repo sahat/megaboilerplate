7f7a3924e9b989df7ec11280846e68aafaa39ac9
Sahat Yalkabov <sakhat@gmail.com>
2015-12-04 22:01:45 -0500

    Refactored generateFramework function to use async/await

---
+++ generators/framework/index.js
@@ -0,0 +1,17 @@
+const generateFrameworkExpress = require('../../generators/framework/express');
+
+async function generateFramework(params) {
+  switch (params.framework) {
+    case 'express':
+      await generateFrameworkExpress(params);
+      break;
+    case 'hapi':
+      break;
+    case 'sails':
+      break;
+    default:
+      break;
+  }
+}
+
+module.exports = generateFramework;
