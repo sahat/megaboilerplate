9fb928c89e669155319491d689efccf05caafa5b
Sahat Yalkabov <sakhat@gmail.com>
2015-12-05 01:13:03 -0500

    Added remaining async functions to download route

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ let prepare = require('./utils/prepare');
 let generateFramework = require('./generators/framework/generateFramework');
 let generateTemplateEngine = require('./generators/template-engine/generateTemplateEngine');
 let generateCssFramework = require('./generators/css-framework/generateCssFramework');
+let generateCssPreprocessor = require('./generators/css-preprocessor/generateCssPreprocessor');
+let generateDatabase = require('./generators/database/generateDatabase');
+let generateAuthentication = require('./generators/authentication/generateAuthentication');
 
 async function download(req, res) {
   let params = await prepare(req.body);
@@ -9,9 +12,10 @@ async function download(req, res) {
   await generateFramework(params);
   await generateTemplateEngine(params);
   await generateCssFramework(params);
-  //await generateCssPreprocessor;
-  //await generateDatabase;
-  //await generateAuthentication;
+  await generateCssPreprocessor(params);
+  await generateDatabase(params);
+  await generateAuthentication(params);
+
   res.end();
 }
 
