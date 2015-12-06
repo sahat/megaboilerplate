29f9b4042d03c03235bcc0cbe0f848de73e34469
Sahat Yalkabov <sakhat@gmail.com>
2015-12-05 23:30:33 -0500

    Framework switch/case for generatePlainCssPreprocessor

--- generators/css-preprocessor/generatePlainCssPreprocessor.js
+++ generators/css-preprocessor/generatePlainCssPreprocessor.js
@@ -4,9 +4,21 @@ let Promise = require('bluebird');
 let copy = Promise.promisify(fs.copy);
 
 async function generatePlainCssPreprocessor(params) {
-  let src = path.join(__base, 'modules', 'css-preprocessor', 'main.css');
-  let dest = path.join(__base, 'build', params.uuid, 'public', 'stylesheets', 'main.css');
-  await copy(src, dest);
+  switch (params.framework) {
+    case 'express':
+      let src = path.join(__base, 'modules', 'css-preprocessor', 'main.css');
+      let dest = path.join(__base, 'build', params.uuid, 'public', 'stylesheets', 'main.css');
+      await copy(src, dest);
+      break;
+    case 'hapi':
+      // TODO
+      break;
+    case 'sails':
+      // TODO
+      break;
+    default:
+    // TODO
+  }
 }
 
 module.exports = generatePlainCssPreprocessor;
