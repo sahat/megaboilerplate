913f177a0b27745abbf40c283ae86bde9a82544c
Sahat Yalkabov <sakhat@gmail.com>
2015-12-06 18:52:36 -0500

    Refactored generatePlainCssPreprocessor

--- generators/css-preprocessor/generatePlainCssPreprocessor.js
+++ generators/css-preprocessor/generatePlainCssPreprocessor.js
@@ -2,17 +2,23 @@ let path = require('path');
 let fs = require('fs-extra');
 let Promise = require('bluebird');
 let copy = Promise.promisify(fs.copy);
+let removeCode = require('../../utils/removeCode');
+let replaceCode = require('../../utils/replaceCode');
+
 
 async function generatePlainCssPreprocessor(params) {
-  let cssPreprocessorModule = path.join(__base, 'modules', 'css-preprocessor');
-  let normalizeCss = path.join(cssPreprocessorModule, 'normalize.css');
-  let mainCss = path.join(cssPreprocessorModule, 'main.css');
+  let build = path.join(__base, 'build', params.uuid);
+  let normalizeCss = path.join(__base, 'modules', 'css-preprocessor', 'normalize.css');
+  let mainCss = path.join(__base, 'modules', 'css-preprocessor', 'main.css');
 
   switch (params.framework) {
     case 'express':
-      let cssDir = path.join(__base, 'build', params.uuid, 'public', 'stylesheets');
-      await copy(normalizeCss, path.join(cssDir, 'normalize.css'));
-      await copy(mainCss, path.join(cssDir, 'main.css'));
+      let layout = path.join(build, 'views', 'layout.jade');
+      let cssImport = path.join(__base, 'modules', 'css-preprocessor', 'express-jade-import.jade');
+
+      await addTemplateImport(params, layout, cssImport);
+      await copy(normalizeCss, path.join(build, 'public', 'stylesheets', 'normalize.css'));
+      await copy(mainCss, path.join(build, 'public', 'stylesheets', 'main.css'));
       break;
     case 'hapi':
       // TODO
@@ -24,5 +30,25 @@ async function generatePlainCssPreprocessor(params) {
     // TODO
   }
 }
+
+// move to utils
+
+async function addTemplateImport(params, layout, templateImport) {
+  switch (params.templateEngine) {
+    case 'jade':
+      await replaceCode(layout, 'CSS_PREPROCESSOR_IMPORT', templateImport, { indentLevel: 2 });
+      break;
+    case 'handlebars':
+      // TODO
+      break;
+    case 'swig':
+      // TODO
+      break;
+    default:
+      // TODO
+  }
+}
+
+
 
 module.exports = generatePlainCssPreprocessor;
