093897050d7db4ef0d436d52d2ce015c6df2c7be
Sahat Yalkabov <sakhat@gmail.com>
2015-12-10 01:48:05 -0500

    Generate custom main.js with jquery support if available

--- generators/js-framework/generateJsFrameworkNone.js
+++ generators/js-framework/generateJsFrameworkNone.js
@@ -6,16 +6,27 @@ let replaceCode = require('../../utils/replaceCode');
 let removeCode = require('../../utils/removeCode');
 let addDependencies = require('../../utils/addDependencies');
 
+const cssFrameworkWithJquery = ['bootstrapCss', 'bootstrapLess', 'bootstrapSass', 'foundationLess', 'foundationSass'];
+
 async function generateJsFrameworkNone(params) {
   let mainJs = path.join(__base, 'modules', 'js-framework', 'none', 'main.js');
+  let mainJsWithJquery = path.join(__base, 'modules', 'js-framework', 'none', 'main-with-jquery.js');
 
   switch (params.framework) {
     case 'express':
       let layout = path.join(__base, 'build', params.uuid, 'views', 'layout.jade');
       let mainJsImport = path.join(__base, 'modules', 'js-framework', 'none', 'express-jade-import.jade');
 
+      // Add HTML references
       await addTemplateImport(params, layout, mainJsImport);
-      await copy(mainJs, path.join(__base, 'build', params.uuid, 'public', 'javascripts', 'main.js'));
+
+      // Copy main.js with jQuery support, if jQuery has been added by other dependencies
+      // such as Bootstrap or Foundation CSS frameworks
+      if (cssFrameworkWithJquery.includes(params.cssFramework)) {
+        await copy(mainJsWithJquery, path.join(__base, 'build', params.uuid, 'public', 'javascripts', 'main.js'));
+      } else {
+        await copy(mainJs, path.join(__base, 'build', params.uuid, 'public', 'javascripts', 'main.js'));
+      }
       break;
     case 'hapi':
       // TODO
