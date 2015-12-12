39de93711a8a9735cda85413375a0003d81a0041
Sahat Yalkabov <sakhat@gmail.com>
2015-12-11 19:27:56 -0500

    Copy gitignore during prepare step

--- utils/prepare.js
+++ utils/prepare.js
@@ -3,10 +3,13 @@ let shortid = require('shortid');
 let fs = require('fs-extra');
 let Promise = require('bluebird');
 let mkdirs = Promise.promisify(fs.mkdirs);
+let copy = Promise.promisify(fs.copy);
 
 async function prepare(params) {
+  let gitignore = path.join(__base, 'modules', 'prepare', '.gitignore');
   params.uuid = shortid.generate();
   await mkdirs(path.join(__base, 'build', params.uuid));
+  await copy(gitignore, path.join(__base, 'build', params.uuid, '.gitignore'));
   return params;
 }
 
