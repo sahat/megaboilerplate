5ea81373851ffef5a13b4ec29d67dce7f02d0f38
Sahat Yalkabov <sakhat@gmail.com>
2015-12-30 16:40:07 -0500

    Moved react-transform options to webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,9 +18,35 @@ module.exports = {
   ],
   module: {
     loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      //// Skip any files outside of these directories
+      //include: [
+      //  path.resolve(__dirname, 'src'),
+      //  path.resolve(__dirname, 'routes.js'),
+      //  path.resolve(__dirname, 'utils'),
+      //  path.resolve(__dirname, 'generators'),
+      //  path.resolve(__dirname, 'modules')
+      //],
+      exclude: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      query: {
+        plugins: [
+          ['react-transform', {
+            transforms: [
+              {
+                transform: 'react-transform-hmr',
+                imports: ['react'],
+                locals: ['module']
+              }, {
+                transform: 'react-transform-catch-errors',
+                imports: ['react', 'redbox-react']
+              }
+            ]
+          }]
+        ]
+      }
     }]
   }
 };
