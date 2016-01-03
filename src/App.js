47261f42172ac2c7b0f2c84c19e255654fcbdf07
Sahat Yalkabov <sakhat@gmail.com>
2016-01-03 15:34:54 -0500

    Added footer, wrapped main content with <main>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Hero from './Hero';
 import Home from './Home';
+import Footer from './Footer';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Hero />
-        {this.props.children}
+        <main>{this.props.children}</main>
+        <Footer/>
       </div>
     );
   }
