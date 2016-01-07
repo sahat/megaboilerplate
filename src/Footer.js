511b963b121225693505a5441ac3ae75c1154124
Sahat Yalkabov <sakhat@gmail.com>
2016-01-07 18:30:31 -0500

    Cleaned up footer text

--- src/Footer.js
+++ src/Footer.js
@@ -10,170 +10,66 @@ class Footer extends React.Component {
             <div className="col-md-3 col-sm-6">
               <div className="widget">
                 <h6 className="title">Recent Posts</h6>
-                  <ul className="link-list recent-posts">
-                    <li>
-                      <a href="#">Hugging pugs is super trendy</a>
+                <ul className="link-list recent-posts">
+                  <li>
+                    <a href="#">Hugging pugs is super trendy</a>
                                         <span className="date">February
                                             <span className="number">14, 2015</span>
                                         </span>
-                    </li>
-                    <li>
-                      <a href="#">Spinning vinyl is oh so cool</a>
+                  </li>
+                  <li>
+                    <a href="#">Spinning vinyl is oh so cool</a>
                                         <span className="date">February
                                             <span className="number">9, 2015</span>
                                         </span>
-                    </li>
-                    <li>
-                      <a href="#">Superior theme design by pros</a>
+                  </li>
+                  <li>
+                    <a href="#">Superior theme design by pros</a>
                                         <span className="date">January
                                             <span className="number">27, 2015</span>
                                         </span>
-                    </li>
-                  </ul>
+                  </li>
+                </ul>
               </div>
             </div>
             <div className="col-md-3 col-sm-6">
               <div className="widget">
                 <h6 className="title">Latest Updates</h6>
-                  <div className="twitter-feed">
-                    <div className="tweets-feed" data-widget-id="492085717044981760" id="tweets-0">
-                      <ul className="slides">
-                        <li>
-                          <div className="user">
-                            <a href="https://twitter.com/mrareweb" aria-label="Medium Rare (screen name: mrareweb)"
-                               data-scribe="element:user_link">
-                              <img alt=""
-                                   data-src-1x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_normal.png"
-                                   data-src-2x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_bigger.png"
-                                   data-scribe="element:avatar" />
+                <div className="twitter-feed">
+                  <div className="tweets-feed" data-widget-id="492085717044981760" id="tweets-0">
+                    <ul className="slides">
+                      <li>
+                        <div className="user">
+                          <a href="https://twitter.com/mrareweb" aria-label="Medium Rare (screen name: mrareweb)"
+                             data-scribe="element:user_link">
+                            <img alt=""
+                                 data-src-1x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_normal.png"
+                                 data-src-2x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_bigger.png"
+                                 data-scribe="element:avatar"/>
     <span>
 
       <span data-scribe="element:name">Medium Rare</span>
     </span>
-                                <span data-scribe="element:screen_name">@mrareweb</span>
+                            <span data-scribe="element:screen_name">@mrareweb</span>
 
-                            </a>
-                          </div>
-                          <p className="tweet">Thanks <a href="https://twitter.com/peternixey"
-                                                     data-mentioned-user-id="2864051" data-scribe="element:mention">@peternixey</a>!
-                            Great to hear :) Variant relies on browser storage. Just export to a .variant file and you
-                            will never lose your work :)</p><p className="timePosted">Posted on 21 Oct</p><p
-                          className="interact"><a href="https://twitter.com/intent/tweet?in_reply_to=656848764875902976"
-                                              className="twitter_reply_icon">Reply</a><a
-                          href="https://twitter.com/intent/retweet?tweet_id=656848764875902976"
-                          className="twitter_retweet_icon">Retweet</a><a
-                          href="https://twitter.com/intent/favorite?tweet_id=656848764875902976"
-                          className="twitter_fav_icon">Favorite</a></p></li>
-                        <li>
-                          <div className="user">
-                            <a href="https://twitter.com/mrareweb" aria-label="Medium Rare (screen name: mrareweb)"
-                               data-scribe="element:user_link">
-                              <img alt=""
-                                   data-src-1x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_normal.png"
-                                   data-src-2x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_bigger.png"
-                                   data-scribe="element:avatar" />
-    <span>
+                          </a>
+                        </div>
+                        <p className="tweet">Thanks <a href="https://twitter.com/peternixey"
+                                                       data-mentioned-user-id="2864051" data-scribe="element:mention">@peternixey</a>!
+                          Great to hear :) Variant relies on browser storage. Just export to a .variant file and you
+                          will never lose your work :)</p><p className="timePosted">Posted on 21 Oct</p><p
+                        className="interact"><a href="https://twitter.com/intent/tweet?in_reply_to=656848764875902976"
+                                                className="twitter_reply_icon">Reply</a><a
+                        href="https://twitter.com/intent/retweet?tweet_id=656848764875902976"
+                        className="twitter_retweet_icon">Retweet</a><a
+                        href="https://twitter.com/intent/favorite?tweet_id=656848764875902976"
+                        className="twitter_fav_icon">Favorite</a></p></li>
 
-      <span data-scribe="element:name">Medium Rare</span>
-    </span>
-                                <span data-scribe="element:screen_name">@mrareweb</span>
 
-                            </a>
-                          </div>
-                          <p className="tweet">Foundry 1.8.0 brings ajax MailChimp &amp; C'Monitor signups, plus a new nav
-                            style and a stylish new fashion home layout: <a href="https://t.co/2AL9td2eO5"
-                                                                            data-expanded-url="http://foundry.mediumra.re"
-                                                                            target="_blank"
-                                                                            title="http://foundry.mediumra.re"
-                                                                            data-scribe="element:url"><span>http://</span><span>foundry.mediumra.re</span><span></span><span><span>&nbsp;</span></span></a>
-                          </p><p className="timePosted">Posted on 21 Oct</p><p className="interact"><a
-                          href="https://twitter.com/intent/tweet?in_reply_to=656840710419689473"
-                          className="twitter_reply_icon">Reply</a><a
-                          href="https://twitter.com/intent/retweet?tweet_id=656840710419689473"
-                          className="twitter_retweet_icon">Retweet</a><a
-                          href="https://twitter.com/intent/favorite?tweet_id=656840710419689473"
-                          className="twitter_fav_icon">Favorite</a></p></li>
-                        <li>
-                          <div className="user">
-                            <a href="https://twitter.com/mrareweb" aria-label="Medium Rare (screen name: mrareweb)"
-                               data-scribe="element:user_link">
-                              <img alt=""
-                                   data-src-1x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_normal.png"
-                                   data-src-2x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_bigger.png"
-                                   data-scribe="element:avatar" />
-    <span>
 
-      <span data-scribe="element:name">Medium Rare</span>
-    </span>
-                                <span data-scribe="element:screen_name">@mrareweb</span>
-
-                            </a>
-                          </div>
-                          <p className="tweet">Machine has been given an overhaul! Check out <a
-                            href="http://t.co/pww2LSncaw" data-expanded-url="http://machine.mediumra.re" target="_blank"
-                            title="http://machine.mediumra.re" data-scribe="element:url"><span>http://</span><span>machine.mediumra.re</span><span></span><span><span>&nbsp;</span></span></a>
-                            to see what's new!</p><p className="timePosted">Posted on 25 Sep</p><p className="interact"><a
-                          href="https://twitter.com/intent/tweet?in_reply_to=647299851093344257"
-                          className="twitter_reply_icon">Reply</a><a
-                          href="https://twitter.com/intent/retweet?tweet_id=647299851093344257"
-                          className="twitter_retweet_icon">Retweet</a><a
-                          href="https://twitter.com/intent/favorite?tweet_id=647299851093344257"
-                          className="twitter_fav_icon">Favorite</a></p></li>
-                        <li>
-                          <div className="user">
-                            <a href="https://twitter.com/mrareweb" aria-label="Medium Rare (screen name: mrareweb)"
-                               data-scribe="element:user_link">
-                              <img alt=""
-                                   data-src-1x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_normal.png"
-                                   data-src-2x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_bigger.png"
-                                   data-scribe="element:avatar" />
-    <span>
-
-      <span data-scribe="element:name">Medium Rare</span>
-    </span>
-                                <span data-scribe="element:screen_name">@mrareweb</span>
-
-                            </a>
-                          </div>
-                          <p className="tweet">Cheers <a href="https://twitter.com/StewartWadden"
-                                                     data-mentioned-user-id="273586913" data-scribe="element:mention">@StewartWadden</a>
-                            , thanks for alerting us! We're rectifying this now :)</p><p className="timePosted">Posted on 17
-                          Sep</p><p className="interact"><a
-                          href="https://twitter.com/intent/tweet?in_reply_to=644631798547382272"
-                          className="twitter_reply_icon">Reply</a><a
-                          href="https://twitter.com/intent/retweet?tweet_id=644631798547382272"
-                          className="twitter_retweet_icon">Retweet</a><a
-                          href="https://twitter.com/intent/favorite?tweet_id=644631798547382272"
-                          className="twitter_fav_icon">Favorite</a></p></li>
-                        <li>
-                          <div className="user">
-                            <a href="https://twitter.com/mrareweb" aria-label="Medium Rare (screen name: mrareweb)"
-                               data-scribe="element:user_link">
-                              <img alt=""
-                                   data-src-1x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_normal.png"
-                                   data-src-2x="https://pbs.twimg.com/profile_images/588890128409473024/6mECDeLZ_bigger.png"
-                                   data-scribe="element:avatar" />
-    <span>
-
-      <span data-scribe="element:name">Medium Rare</span>
-    </span>
-                                <span data-scribe="element:screen_name">@mrareweb</span>
-
-                            </a>
-                          </div>
-                          <p className="tweet">True <a href="https://twitter.com/mvrhd" data-mentioned-user-id="20759779"
-                                                   data-scribe="element:mention">@mvrhd</a>, you're right - we mean that
-                            there is no FTP required to get started... We'll fix that now :)</p><p className="timePosted">
-                          Posted on 10 Sep</p><p className="interact"><a
-                          href="https://twitter.com/intent/tweet?in_reply_to=642115514441961472"
-                          className="twitter_reply_icon">Reply</a><a
-                          href="https://twitter.com/intent/retweet?tweet_id=642115514441961472"
-                          className="twitter_retweet_icon">Retweet</a><a
-                          href="https://twitter.com/intent/favorite?tweet_id=642115514441961472"
-                          className="twitter_fav_icon">Favorite</a></p></li>
-                      </ul>
-                    </div>
+                    </ul>
                   </div>
+                </div>
               </div>
             </div>
             <div className="col-md-3 col-sm-6">
