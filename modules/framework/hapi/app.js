7acf69d07abca44d9741ae6fc39cb3e9e342cb54
Sahat Yalkabov <sakhat@gmail.com>
2016-01-09 20:05:09 -0500

    Added host to hapi.js module when starting a server

--- modules/framework/hapi/app.js
+++ modules/framework/hapi/app.js
@@ -1,7 +1,10 @@
 var Hapi = require('hapi');
 
 var server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({
+  host: 'localhost',
+  port: 3000
+});
 
 server.start(function() {
   console.log('Server running at:', server.info.uri);
