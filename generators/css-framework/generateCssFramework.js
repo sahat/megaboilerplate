2883a9e4a3c3e502634ddcfeb4226271e76aa714
Sahat Yalkabov <sakhat@gmail.com>
2016-01-14 21:42:56 -0500

    Use cssPreprocessor instead of cssFrameworkOptions

--- generators/css-framework/generateCssFramework.js
+++ generators/css-framework/generateCssFramework.js
@@ -1,20 +1,21 @@
 import generateBootstrapCss from './generateBootstrapCss';
+import generateBootstrapLess from './generateBootstrapLess';
 import generateCssFrameworkNone from './generateCssFrameworkNone';
 
 async function generateCssFramework(params) {
-  if (params.cssFramework === 'bootstrap' && params.cssFrameworkOptions === 'css') {
+  if (params.cssFramework === 'bootstrap' && params.cssPreprocessor === 'css') {
     await generateBootstrapCss(params);
-  } else if (params.cssFramework === 'bootstrap' && params.cssFrameworkOptions === 'less') {
+  } else if (params.cssFramework === 'bootstrap' && params.cssPreprocessor === 'less') {
+    await generateBootstrapLess(params);
+  } else if (params.cssFramework === 'bootstrap' && params.cssPreprocessor === 'sass') {
     // TODO
-  } else if (params.cssFramework === 'bootstrap' && params.cssFrameworkOptions === 'sass') {
+  } else if (params.cssFramework === 'foundation' && params.cssPreprocessor === 'css') {
     // TODO
-  } else if (params.cssFramework === 'foundation' && params.cssFrameworkOptions === 'css') {
+  } else if (params.cssFramework === 'foundation' && params.cssPreprocessor === 'sass') {
     // TODO
-  } else if (params.cssFramework === 'foundation' && params.cssFrameworkOptions === 'sass') {
+  } else if (params.cssFramework === 'bourbonNeat' && params.cssPreprocessor === 'css') {
     // TODO
-  } else if (params.cssFramework === 'bourbonNeat' && params.cssFrameworkOptions === 'css') {
-    // TODO
-  } else if (params.cssFramework === 'bourbonNeat' && params.cssFrameworkOptions === 'sass') {
+  } else if (params.cssFramework === 'bourbonNeat' && params.cssPreprocessor === 'sass') {
     // TODO
   } else if (params.cssFramework === 'none') {
     // TODO
