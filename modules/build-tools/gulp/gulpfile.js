8e7e32dc126b8a66af278e2c4eeb3e0f25f31f70
Sahat Yalkabov <sakhat@gmail.com>
2016-01-15 21:31:05 -0500

    Updated gulpfile module

--- modules/build-tools/gulp/gulpfile.js
+++ modules/build-tools/gulp/gulpfile.js
@@ -1,42 +1,13 @@
 var gulp = require('gulp');
-var coffee = require('gulp-coffee');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
-var del = require('del');
-
+//= LESS_GULP_REQUIRE
+//= SASS_GULP_REQUIRE
 var paths = {
   scripts: ['client/js/**/*.coffee', '!client/external/**/*.coffee'],
   images: 'client/img/**/*'
 };
-
-// Not all tasks need to use streams
-// A gulpfile is just another node program and you can use any package available on npm
-gulp.task('clean', function() {
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  return del(['build']);
-});
-
-gulp.task('scripts', ['clean'], function() {
-  // Minify and copy all JavaScript (except vendor scripts)
-  // with sourcemaps all the way down
-  return gulp.src(paths.scripts)
-    .pipe(sourcemaps.init())
-    .pipe(coffee())
-    .pipe(uglify())
-    .pipe(concat('all.min.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('build/js'));
-});
-
-// Copy all static images
-gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
-    // Pass in options to the task
-    .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img'));
-});
+//= LESS_GULP_TASK
+//= SASS_GULP_TASK
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
