fa72effedbd39090d98b305eee135522c247e233
Sahat Yalkabov <sakhat@gmail.com>
2016-01-22 02:55:41 -0500

    Simplified footer further, one-liner

--- src/Footer.js
+++ src/Footer.js
@@ -3,16 +3,8 @@ import { Router, Route, Link } from 'react-router'
 
 const Footer = (props) => {
     return (
-      <footer className="text-center">
-        <p>
-          Mega Boilerplate <span className="hide-xs">·</span> Created by <a href="http://sahatyalkabov.com">Sahat Yalkabov</a>
-        </p>
-        <p>
-          Code licensed under <a href="http://opensource.org/licenses/mit-license.html">MIT License</a>
-        </p>
-        <p>
-          <a href="https://github.com/sahat/boilerplate">GitHub Project</a> · <a href="https://github.com/sahat/boilerplate/issues">Issues</a>
-        </p>
+      <footer className="container">
+        © 2016 <a href="http://sahatyalkabov.com">Sahat Yalkabov</a>. MIT Licensed.
       </footer>
     );
 };
