11040e77998aca3dcfdd08f5e4a6913f3ef5b548
Sahat Yalkabov <sakhat@gmail.com>
2016-01-24 20:12:51 -0500

    CSS framework generator refactoring

--- generators/css-framework/generateCssFramework.js
+++ generators/css-framework/generateCssFramework.js
@@ -2,11 +2,24 @@ import generateBootstrap from './generateBootstrap';
 import generateCssFrameworkNone from './generateCssFrameworkNone';
 
 async function generateCssFramework(params) {
-  if (params.cssFramework === 'bootstrap') {
-    await generateBootstrap(params);
-  } else if (params.cssFramework === 'foundation') {
-  } else if (params.cssFramework === 'bourbonNeat') {
-  } else if (params.cssFramework === 'none') {
+  console.log(params.cssFramework);
+  switch (params.cssFramework) {
+    case 'bootstrap':
+      await generateBootstrap(params);
+      break;
+
+    case 'foundation':
+      break;
+
+    case 'bourbonNeat':
+      break;
+
+    case 'none':
+      await generateCssFrameworkNone(params);
+      break;
+
+    default:
+      break;
   }
 }
 
--- generators/css-framework/generateCssFrameworkNone.js
+++ generators/css-framework/generateCssFrameworkNone.js
@@ -1,33 +1,31 @@
 import { join } from 'path';
-import { copy, replaceCode, removeCode } from '../utils';
+import { cpy, replaceCode } from '../utils';
 
 async function generateCssFrameworkNone(params) {
-  let build = join(__base, 'build', params.uuid);
-  let normalizeCss = join(__base, 'modules', 'css-framework', 'none', 'normalize.css');
-  let token = 'CSS_FRAMEWORK_IMPORT';
-  let indentOptions = { indentLevel: 2 };
+  const build = join(__base, 'build', params.uuid);
+  const normalizeCss = join(__base, 'modules', 'css-framework', 'none', 'normalize.css');
+  const token = 'CSS_FRAMEWORK_IMPORT';
 
   switch (params.framework) {
     case 'express':
-      let layout = join(build, 'views', 'layout.jade');
-
+    case 'hapi':
       if (params.templateEngine === 'jade') {
-        let cssImport = join(__base, 'modules', 'css-framework', 'none', 'express-jade-import.jade');
-        await replaceCode(layout, token, cssImport, indentOptions);
+        const layoutJade = join(build, 'views', 'layout.jade');
+        const cssImport = join(__base, 'modules', 'css-framework', 'none', 'express-jade-import.jade');
+        await replaceCode(layoutJade, token, cssImport, { indentLevel: 2 });
       } else if (params.templateEngine === 'handlebars') {
         // TODO
       } else if (params.templateEngine === 'nunjucks') {
         // TODO
       }
+      // Copy normalize.css
+      await cpy([normalizeCss], join(build, 'public', 'stylesheets'));
+      break;
 
-      await copy(normalizeCss, join(build, 'public', 'stylesheets', 'normalize.css'));
-      break;
-    case 'hapi':
-      // TODO
-      break;
     case 'meteor':
       // TODO
       break;
+
     default:
     // TODO
   }
