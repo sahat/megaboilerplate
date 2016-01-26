31b8fe83ad9f7a72240ce7873c7ad478c5e648c0
Sahat Yalkabov <sakhat@gmail.com>
2016-01-26 00:32:12 -0500

    Removed watchify and uglify from react's gulp require

--- modules/build-tool/gulp/react-gulp-require.js
+++ modules/build-tool/gulp/react-gulp-require.js
@@ -2,5 +2,3 @@ var gutil = require('gulp-util');
 var source = require('vinyl-source-stream');
 var babelify = require('babelify');
 var browserify = require('browserify');
-var watchify = require('watchify');
-var uglify = require('gulp-uglify');
