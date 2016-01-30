b84e50c9c35f81b737b7ed68f2581ed44e8957cf
Sahat Yalkabov <sakhat@gmail.com>
2016-01-30 18:35:12 -0500

    Updated bootstrap generator to handle nunjucks templates

--- generators/css-framework/generateBootstrap.js
+++ generators/css-framework/generateBootstrap.js
@@ -2,21 +2,30 @@ import { join } from 'path';
 import { copy, replaceCode } from '../utils';
 
 async function generateBootstrap(params) {
-  let bootstrapDir = join(__base, 'modules', 'css-framework', 'bootstrap');
-  let jqueryDir = join(__base, 'modules', 'css-framework', 'jquery');
-  let publicDir = join(__base, 'build', params.uuid, 'public');
+  const bootstrapDir = join(__base, 'modules', 'css-framework', 'bootstrap');
+  const jqueryDir = join(__base, 'modules', 'css-framework', 'jquery');
+  const publicDir = join(__base, 'build', params.uuid, 'public');
 
   // Add CSS import
   switch (params.templateEngine) {
     case 'jade':
-      let layout = join(__base, 'build', params.uuid, 'views', 'layout.jade');
-      let cssImport = join(__base, 'modules', 'css-framework', 'bootstrap', 'jade-import.jade');
-      await replaceCode(layout, 'CSS_FRAMEWORK_IMPORT', cssImport, { indentLevel: 2 });
+      const jadeLayout = join(__base, 'build', params.uuid, 'views', 'layout.jade');
+      const jadeBootstrapImport = join(__base, 'modules', 'css-framework', 'bootstrap', 'jade-import.jade');
+      await replaceCode(jadeLayout, 'CSS_FRAMEWORK_IMPORT', jadeBootstrapImport, { indentLevel: 2 });
       break;
+
     case 'handlebars':
+      const handlebarsLayout = join(__base, 'build', params.uuid, 'views', 'layout.html');
+      const handlebarsBootstrapImport = join(__base, 'modules', 'css-framework', 'bootstrap', 'html-import.html');
+      await replaceCode(handlebarsLayout, 'CSS_FRAMEWORK_IMPORT', handlebarsBootstrapImport, { indentLevel: 1 });
       break;
+
     case 'nunjucks':
+      const nunjucksLayout = join(__base, 'build', params.uuid, 'views', 'layout.html');
+      const nunjucksBootstrapImport = join(__base, 'modules', 'css-framework', 'bootstrap', 'html-import.html');
+      await replaceCode(nunjucksLayout, 'CSS_FRAMEWORK_IMPORT', nunjucksBootstrapImport, { indentLevel: 1 });
       break;
+
     default:
       break;
   }
