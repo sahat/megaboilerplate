f9aa29930b955cd04676dfa7c64d1638cd67724a
Sahat Yalkabov <sakhat@gmail.com>
2016-02-04 20:43:50 -0500

    Improved react options dropdown

--- site/sections/JsFramework.js
+++ site/sections/JsFramework.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import cx from 'classnames';
 import { capitalize } from 'lodash/string';
+import { VelocityComponent } from 'velocity-react';
 
 const JS_FRAMEWORK_SVG = (
   <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 50 50">
@@ -9,122 +10,136 @@ const JS_FRAMEWORK_SVG = (
 );
 
 class JsFramework extends React.Component {
- render() {
-   let props = this.props;
-   let description;
-   let jsFrameworkOptions;
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.toggleAdditionalOptions = this.toggleAdditionalOptions.bind(this);
+  }
 
-   switch (props.jsFramework) {
-     case 'react':
-       description = (
-         <div>
-           <strong><a href="https://facebook.github.io/react/" target="_blank">React</a></strong> — A  Library for building UIs. Advantages: simple, declarative, composable components, Virtual DOM, one-way reactive data-flow.
-         </div>
-       );
-       jsFrameworkOptions = (
-         <div className="panel-collapse">
-           <div className="checkbox">
-             <label cl>
-               <input type="checkbox" name="reactOptionsCheckboxes" value="redux" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('redux')} />
-               <span className="hint--top hint--rounded" data-hint="Predictable state container for JavaScript apps. Redux is an evolution of the Facebook's Flux, without all the complexity.">Redux</span>
-             </label>
-           </div>
-           <div className="checkbox">
-             <label>
-               <input type="checkbox" name="reactOptionsCheckboxes" value="reactRouter" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('reactRouter')} />
-               <span className="hint--top hint--rounded" data-hint="A complete routing library for React. React Router keeps your UI in sync with the URL.">React Router</span>
-             </label>
-           </div>
-           <div className="checkbox">
-             <label>
-               <input type="checkbox" name="reactOptionsCheckboxes" value="graphql" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('graphql')} />
-               <span className="hint--top hint--rounded" data-hint="Relay is a framework from Facebook that provides data-fetching functionality for React applications.">GraphQL + Relay</span>
-             </label>
-           </div>
-           <div className="checkbox">
-             <label>
-               <input type="checkbox" name="reactOptionsCheckboxes" value="es6" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('es6')} />
-               <span className="hint--top hint--rounded" data-hint="ECMAScript 2015 (ES6) is the upcoming sixth major release of the ECMAScript language specification.">ES6</span>
-             </label>
-           </div>
-           <div className="checkbox">
-             <label>
-               <input type="checkbox" name="reactOptionsCheckboxes" value="hotReload" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('hotReload')} />
-               <span className="hint--top hint--rounded" data-hint="Tweak React components in real time, while preserving the state.">React Hot Reload</span>
-             </label>
-           </div>
-         </div>
-       );
-       break;
-     case 'angular':
-       description = (
-         <div>
-           <strong><a href="https://angular.io/" target="_blank">Angular 2</a></strong> — A JavaScript framework for building web apps. Advantages: feature-rich, big community, two-way data binding, MVC architecture.
-         </div>
-       );
-       jsFrameworkOptions = null;
-       break;
-     default:
-       description = <div className="placeholder"> </div>;
-       jsFrameworkOptions = null;
-   }
+  toggleAdditionalOptions() {
+    this.setState({ showOptions: !this.state.showOptions });
+  }
 
-   let optionsClasses = cx("nav nav-stacked", {
-     fadeIn: props.jsFramework && props.jsFramework !== 'none',
-     animated: props.jsFramework && props.jsFramework !== 'none',
-     invisible: !props.jsFramework || props.jsFramework === 'none'
-   });
+  render() {
+    const props = this.props;
+    const state = this.state;
+    let description;
+    let jsFrameworkOptions;
 
-   const recommended = props.beginner ? (
-     <span className="hint--top hint--rounded" data-hint="Recommended">
-        <img src="/img/svg/recommended.svg" alt="Recommended" />
+    switch (props.jsFramework) {
+      case 'react':
+        description = (
+          <div>
+            <strong><a href="https://facebook.github.io/react/" target="_blank">React</a></strong> — A Library for building UIs. Advantages: simple, declarative, composable components, Virtual DOM, one-way reactive data-flow.
+          </div>
+        );
+        break;
+      case 'angular':
+        description = (
+          <div>
+            <strong><a href="https://angular.io/" target="_blank">Angular 2</a></strong> — A JavaScript framework for building web apps. Advantages: feature-rich, big community, two-way data binding, MVC architecture.
+          </div>
+        );
+        break;
+      default:
+        description = <div className="placeholder"></div>;
+    }
+
+    const additionalOptions = (state.showOptions && props.jsFramework === 'react') ? (
+      <div>
+        <VelocityComponent runOnMount animation="transition.slideUpIn">
+          <div className="checkbox transparent">
+            <label className="hint--right hint--rounded" data-hint="Predictable state container for JavaScript apps. Redux is an evolution of the Facebook's Flux, without all the complexity.">
+              <input type="checkbox"  name="reactOptionsCheckboxes" value="reactRouter" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('redux')}/>
+              <span><img className="icon" src="/img/svg/flux-logo.svg" alt="Redux"/> Redux</span>
+            </label>
+          </div>
+        </VelocityComponent>
+        <VelocityComponent runOnMount animation="transition.slideUpIn" delay={75}>
+          <div className="checkbox transparent">
+            <label className="hint--right hint--rounded" data-hint="A complete routing library for React. React Router keeps your UI in sync with the URL.">
+              <input type="checkbox" name="reactOptionsCheckboxes" value="reactRouter" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('reactRouter')}/>
+              <span>React Router</span>
+            </label>
+          </div>
+        </VelocityComponent>
+        <VelocityComponent runOnMount animation="transition.slideUpIn" delay={150}>
+          <div className="checkbox transparent">
+            <label className="hint--right hint--rounded" data-hint="Relay is a framework from Facebook that provides data-fetching functionality for React applications.">
+              <input type="checkbox" name="reactOptionsCheckboxes" value="graphql" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('graphql')}/>
+              <span><img className="icon" src="/img/svg/relay-logo.svg" alt="Relay"/> GraphQL + Relay</span>
+            </label>
+          </div>
+        </VelocityComponent>
+        <VelocityComponent runOnMount animation="transition.slideUpIn" delay={225}>
+          <div className="checkbox transparent">
+            <label className="hint--right hint--rounded" data-hint="ECMAScript 2015 (ES6) is the upcoming sixth major release of the ECMAScript language specification.">
+              <input type="checkbox" name="reactOptionsCheckboxes" value="es6" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('es6')}/>
+              <span>ES6</span>
+            </label>
+          </div>
+        </VelocityComponent>
+        <VelocityComponent runOnMount animation="transition.slideUpIn" delay={300}>
+          <div className="checkbox transparent">
+            <label className="hint--right hint--rounded" data-hint="Tweak React components in real time, while preserving the state.">
+              <input type="checkbox" name="reactOptionsCheckboxes" value="hotReload" onChange={props.handleChange} checked={props.reactOptions && props.reactOptions.has('hotReload')}/>
+              <span>React Transform HMR</span>
+            </label>
+          </div>
+        </VelocityComponent>
+      </div>
+    ) : null;
+
+    const additionalOptionsButton = props.jsFramework ? (
+      <div>
+        <span className="options" onClick={this.toggleAdditionalOptions}>
+          <img className={cx('animated', { fast: state.showOptions })} src="/img/svg/options.svg"/>
+          <span>Additional Options</span>
+        </span>
+        {additionalOptions}
+      </div>
+    ) : null;
+
+    const recommended = props.beginner ? (
+      <span className="hint--top hint--rounded" data-hint="Recommended">
+        <img src="/img/svg/recommended.svg" alt="Recommended"/>
       </span>
-   ) : null;
+    ) : null;
 
-   return (
-     <div className={cx('zoomInBackwards panel', props.jsFramework)}>
-       <div className="panel-heading">
-         <h6>{JS_FRAMEWORK_SVG}{!props.jsFramework || props.jsFramework === 'none' ? 'JavaScript Framework' : props.jsFramework}</h6>
-         <a href="#" className="stats-icon hint--top hint--rounded" data-hint="Download Stats">
-           <i className="fa fa-bar-chart" />
-         </a>
-       </div>
-       <div className="panel-body">
-         {description}
-         <div className="radio-group">
-           <label className="radio-inline">
-             <img className="btn-logo" src="/img/svg/none.png" alt="None" />
-             <input type="radio" name="jsFrameworkRadios" value="none" onChange={props.handleChange} checked={props.jsFramework === 'none'} />
-             <span>None</span>
-             {recommended}
-           </label>
-           <label className="radio-inline">
-             <img className="btn-logo" src="/img/svg/react-logo.svg" alt="React" />
-             <input type="radio" name="jsFrameworkRadios" value="react" onChange={props.handleChange} checked={props.jsFramework === 'react'} />
-             <span>React</span>
-           </label>
-           <label className="radio-inline">
-             <img className="btn-logo" src="/img/svg/angular2.png" alt="Angular" />
-             <input type="radio" name="jsFrameworkRadios" value="angular" onChange={props.handleChange} checked={props.jsFramework === 'angular'} />
-             <span>Angular 2</span>
-           </label>
-         </div>
+    return (
+      <div className={cx('zoomInBackwards panel', props.jsFramework)}>
+        <div className="panel-heading">
+          <h6>{JS_FRAMEWORK_SVG}{!props.jsFramework || props.jsFramework === 'none' ? 'JavaScript Framework' : props.jsFramework}</h6>
+          <a href="#" className="stats-icon hint--top hint--rounded" data-hint="Download Stats">
+            <i className="fa fa-bar-chart"/>
+          </a>
+        </div>
+        <div className="panel-body">
+          {description}
+          <div className="radio-group">
+            <label className="radio-inline">
+              <img className="btn-logo" src="/img/svg/none.png" alt="None"/>
+              <input type="radio" name="jsFrameworkRadios" value="none" onChange={props.handleChange} checked={props.jsFramework === 'none'}/>
+              <span>None</span>
+              {recommended}
+            </label>
+            <label className="radio-inline">
+              <img className="btn-logo" src="/img/svg/react-logo.svg" alt="React"/>
+              <input type="radio" name="jsFrameworkRadios" value="react" onChange={props.handleChange} checked={props.jsFramework === 'react'}/>
+              <span>React</span>
+            </label>
+            <label className="radio-inline">
+              <img className="btn-logo" src="/img/svg/angular2.png" alt="Angular"/>
+              <input type="radio" name="jsFrameworkRadios" value="angular" onChange={props.handleChange} checked={props.jsFramework === 'angular'}/>
+              <span>Angular 2</span>
+            </label>
+          </div>
 
-         <ul className={optionsClasses}>
-           <li>
-             <a data-toggle="collapse" href="#jsFrameworkCollapse1">
-               <img className="options-icon animated" src="/img/svg/options.svg"/>
-               {`Additional Options for ${capitalize(props.jsFramework)}`}
-             </a>
-             <div id="jsFrameworkCollapse1" className="collapse">
-               {jsFrameworkOptions}
-             </div>
-           </li>
-         </ul>
-       </div>
-     </div>
-   );
- }
+          {additionalOptionsButton}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default JsFramework;
