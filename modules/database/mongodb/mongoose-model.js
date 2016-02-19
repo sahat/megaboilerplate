6c1a638d128ee63571cdf708927ef11f1547e4f2
Sahat Yalkabov <sakhat@gmail.com>
2016-02-19 18:27:24 -0500

    Updated user mongoose model and schema

--- modules/database/mongodb/mongoose-model.js
+++ modules/database/mongodb/mongoose-model.js
@@ -1,1 +1,35 @@
-var User = mongoose.model('User', { name: String });
+var bcrypt = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: String,
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  passwordResetToken: String,
+  passwordResetExpires: Date
+});
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) { return next(); }
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) { return next(err); }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return cb(err); }
+    cb(null, isMatch);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
