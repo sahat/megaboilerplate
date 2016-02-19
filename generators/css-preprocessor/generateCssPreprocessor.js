8afd5c36434921c46918145a22f0165bda63e9b5
Sahat Yalkabov <sakhat@gmail.com>
2016-02-19 13:45:18 -0500

    Generate main.css via cssPreprocessor only if css framework is not set

--- generators/css-preprocessor/generateCssPreprocessor.js
+++ generators/css-preprocessor/generateCssPreprocessor.js
@@ -3,19 +3,21 @@ import generateSassPreprocessor from './generateSassPreprocessor';
 import generateLessPreprocessor from './generateLessPreprocessor';
 
 async function generateCssPreprocessor(params) {
-  switch (params.cssPreprocessor) {
-    case 'css':
-      await generatePlainCssPreprocessor(params);
-      break;
-    case 'sass':
-      await generateSassPreprocessor(params);
-      break;
-    case 'less':
-      await generateLessPreprocessor(params);
-      break;
-    case 'stylus':
-      break;
-    default:
+  if (params.cssFramework === 'none') {
+    switch (params.cssPreprocessor) {
+      case 'css':
+        await generatePlainCssPreprocessor(params);
+        break;
+      case 'sass':
+        await generateSassPreprocessor(params);
+        break;
+      case 'less':
+        await generateLessPreprocessor(params);
+        break;
+      case 'stylus':
+        break;
+      default:
+    }
   }
 }
 
