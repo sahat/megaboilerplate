419fe8f55be6ee749aa4424a9a9e33931f867e7c
Sahat Yalkabov <sakhat@gmail.com>
2016-02-19 22:28:17 -0500

    Updated local strategy

--- modules/authentication/local/passport-strategy.js
+++ modules/authentication/local/passport-strategy.js
@@ -1,14 +1,14 @@
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, 'Incorrect username or password.');
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, 'Incorrect username or password.');
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+  User.findOne({ email: email.toLowerCase() }, function(err, user) {
+    if (!user) {
+      return done(null, false, 'The email address ' + email + ' is not associated with any account. ' +
+        'Double-check your email address and try again.');
+    }
+    user.comparePassword(password, function(err, isMatch) {
+      if (!isMatch) {
+        return done(null, false, { message: 'Invalid email or password.' });
       }
       return done(null, user);
     });
-  }
-));
+  });
+}));
