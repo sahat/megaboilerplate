3f3264f6c2c8aa61d628627cf1bf2fcd3c4f0784
Sahat Yalkabov <sakhat@gmail.com>
2016-02-19 22:24:50 -0500

    Renamed email to local auth strategy folder name

--- generators/authentication/generateEmailAuthenticationExpress.js
+++ generators/authentication/generateEmailAuthenticationExpress.js
@@ -3,9 +3,9 @@ import { replaceCode, removeCode, addNpmPackage } from '../utils';
 
 async function generateEmailAuthenticationExpress(params) {
   const config = join(__base, 'build', params.uuid, 'config', 'passport.js');
-  const require = join(__base, 'modules', 'authentication', 'email', 'passport-require.js');
-  const strategy = join(__base, 'modules', 'authentication', 'email', 'passport-strategy.js');
-  const routes = join(__base, 'modules', 'authentication', 'email', 'passport-routes.js');
+  const require = join(__base, 'modules', 'authentication', 'local', 'passport-require.js');
+  const strategy = join(__base, 'modules', 'authentication', 'local', 'passport-strategy.js');
+  const routes = join(__base, 'modules', 'authentication', 'local', 'passport-routes.js');
 
   await replaceCode(config, 'PASSPORT_LOCAL_REQUIRE', require);
   await replaceCode(config, 'PASSPORT_LOCAL_STRATEGY', strategy);
