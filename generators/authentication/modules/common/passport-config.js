4cbafb27a84d0b11b3c20e8aed8e3405d41db7cd
Sahat Yalkabov <sakhat@gmail.com>
2016-03-03 13:27:29 -0500

    Added isAuthenticated and isAuthorized middleware to passportConfig

--- generators/authentication/modules/common/passport-config.js
+++ generators/authentication/modules/common/passport-config.js
@@ -12,3 +12,26 @@ var passport = require('passport');
 //= PASSPORT_FACEBOOK_STRATEGY
 //= PASSPORT_GOOGLE_STRATEGY
 //= PASSPORT_TWITTER_STRATEGY
+
+/**
+ * Login Required middleware.
+ */
+exports.isAuthenticated = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
+/**
+ * Authorization Required middleware.
+ */
+exports.isAuthorized = function(req, res, next) {
+  var provider = req.path.split('/').slice(-1)[0];
+
+  if (_.find(req.user.tokens, { kind: provider })) {
+    next();
+  } else {
+    res.redirect('/auth/' + provider);
+  }
+};
