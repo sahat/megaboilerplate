6c7b744a0b6a6c0795dd2daec38c93cb831f1b09
Sahat Yalkabov <sakhat@gmail.com>
2016-03-03 23:18:44 -0500

    Updated local passport strategy and routes

--- generators/authentication/modules/local/passport-strategy.js
+++ generators/authentication/modules/local/passport-strategy.js
@@ -2,12 +2,12 @@
 passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
   User.findOne({ email: email.toLowerCase() }, function(err, user) {
     if (!user) {
-      return done(null, false, 'The email address ' + email + ' is not associated with any account. ' +
-        'Double-check your email address and try again.');
+      return done(null, false, { msg: 'The email address ' + email + ' is not associated with any account. ' +
+      'Double-check your email address and try again.' });
     }
     user.comparePassword(password, function(err, isMatch) {
       if (!isMatch) {
-        return done(null, false, 'Invalid email or password');
+        return done(null, false, { msg: 'Invalid email or password' });
       }
       return done(null, user);
     });
