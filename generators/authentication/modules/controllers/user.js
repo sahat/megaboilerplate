ded24db9058f0d78be6a39dad52df4be1a187fc3
Sahat Yalkabov <sakhat@gmail.com>
2016-03-03 19:44:14 -0500

    loginPost controller refactoring + improved validation

--- generators/authentication/modules/controllers/user.js
+++ generators/authentication/modules/controllers/user.js
@@ -22,29 +22,27 @@ exports.loginGet = function(req, res) {
  */
 exports.loginPost = function(req, res, next) {
   req.assert('email', 'Email is not valid').isEmail();
-  req.assert('password', 'Password cannot be blank').notEmpty();
+  req.assert('email', 'Email cannot be empty').notEmpty();
+  req.assert('password', 'Password cannot be empty').notEmpty();
 
-  var errors = req.validationErrors();
+  var errors = req.validationErrors(true);
 
   if (errors) {
-    req.flash('errors', errors);
-    return res.redirect('/login');
+    return res.render('account/login', {
+      title: 'Log in',
+      errors: errors
+    });
   }
 
   passport.authenticate('local', function(err, user, info) {
-    if (err) {
-      return next(err);
-    }
     if (!user) {
-      req.flash('errors', { msg: info.message });
-      return res.redirect('/login');
+      return res.render('account/login', {
+        title: 'Log in',
+        errors: { user: { msg: info }}
+      });
     }
-    req.logIn(user, function(err) {
-      if (err) {
-        return next(err);
-      }
-      req.flash('success', { msg: 'Success! You are logged in.' });
-      res.redirect(req.session.returnTo || '/');
+    req.logIn(user, function() {
+      res.redirect('/');
     });
   })(req, res, next);
 };
