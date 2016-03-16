9f5c4c9eda87f8d43b0ddaf74ecb6f9e348b99f3
Sahat Yalkabov <sakhat@gmail.com>
2016-03-16 19:14:26 -0400

    Added polymer to js framework choices

--- site/sections/JsFramework.js
+++ site/sections/JsFramework.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import cx from 'classnames';
-import { capitalize } from 'lodash/string';
 import { VelocityComponent } from 'velocity-react';
 
 const JS_FRAMEWORK_SVG = (
   <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 50 50">
-    <path d="M 29.125 7.34375 L 17.125 41.34375 L 20.875 42.65625 L 32.875 8.65625 L 29.125 7.34375 z M 9.9375 13.375 L 1.25 23.71875 L 0.1875 25 L 1.25 26.28125 L 9.9375 36.65625 L 13.03125 34.09375 L 5.40625 25 L 13 15.9375 L 9.9375 13.375 z M 40.0625 13.375 L 37 15.9375 L 44.59375 25 L 37 34.0625 L 40.09375 36.625 L 48.71875 26.28125 L 49.78125 25 L 48.71875 23.71875 L 40.0625 13.375 z" color="#000" overflow="visible"></path>
+    <path d="M 29.125 7.34375 L 17.125 41.34375 L 20.875 42.65625 L 32.875 8.65625 L 29.125 7.34375 z M 9.9375 13.375 L 1.25 23.71875 L 0.1875 25 L 1.25 26.28125 L 9.9375 36.65625 L 13.03125 34.09375 L 5.40625 25 L 13 15.9375 L 9.9375 13.375 z M 40.0625 13.375 L 37 15.9375 L 44.59375 25 L 37 34.0625 L 40.09375 36.625 L 48.71875 26.28125 L 49.78125 25 L 48.71875 23.71875 L 40.0625 13.375 z" overflow="visible"></path>
   </svg>
 );
 
@@ -24,7 +23,6 @@ class JsFramework extends React.Component {
     const props = this.props;
     const state = this.state;
     let description;
-    let jsFrameworkOptions;
 
     switch (props.jsFramework) {
       case 'react':
@@ -132,6 +130,11 @@ class JsFramework extends React.Component {
               <img className="btn-logo" src="/img/svg/angular2.png" alt="Angular"/>
               <input type="radio" name="jsFrameworkRadios" value="angular" onChange={props.handleChange} checked={props.jsFramework === 'angular'}/>
               <span>Angular 2</span>
+            </label>
+            <label className="radio-inline">
+              <img className="btn-logo" src="/img/svg/polymer-logo.svg" alt="Polymer"/>
+              <input type="radio" name="jsFrameworkRadios" value="polymer" onChange={props.handleChange} checked={props.jsFramework === 'polymer'}/>
+              <span>Polymer</span>
             </label>
           </div>
 
