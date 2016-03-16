d57d3fe66eb78d8704fdb514cc915a5b490ea424
Sahat Yalkabov <sakhat@gmail.com>
2016-03-16 02:19:25 -0400

    Cleaned up on-save and compareMethods in mongo user model

--- generators/authentication/modules/models/mongodb/user.js
+++ generators/authentication/modules/models/mongodb/user.js
@@ -26,19 +26,16 @@ userSchema.pre('save', function(next) {
   var user = this;
   if (!user.isModified('password')) { return next(); }
   bcrypt.genSalt(10, function(err, salt) {
-    if (err) { return next(err); }
     bcrypt.hash(user.password, salt, null, function(err, hash) {
-      if (err) { return next(err); }
       user.password = hash;
       next();
     });
   });
 });
 
-userSchema.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) { return cb(err); }
-    cb(null, isMatch);
+userSchema.methods.comparePassword = function(password, cb) {
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    cb(err, isMatch);
   });
 };
 
