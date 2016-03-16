ef0e49cf9eafdbcd5cf4305f2093358fe5a94a79
Sahat Yalkabov <sakhat@gmail.com>
2016-03-16 17:29:54 -0400

    Fixed resetPost query (sql)

--- generators/authentication/modules/controllers/sql/user-reset-post.js
+++ generators/authentication/modules/controllers/sql/user-reset-post.js
@@ -1,15 +1,17 @@
 new User({ passwordResetToken: req.params.token })
-  .where('passwordResetExpires', '>', Date.now())
+  .where('passwordResetExpires', '>', new Date())
   .fetch()
   .then(function(user) {
     if (!user) {
       req.flash('error', { msg: 'Password reset token is invalid or has expired.' });
       return res.redirect('back');
     }
-    user.save({ password: req.body.password, passwordResetToken: null, passwordResetExpires: null }, { patch: true })
-      .then(function() {
-        req.logIn(user, function(err) {
-          done(err, user);
-        });
+    user.set('password', req.body.password);
+    user.set('passwordResetToken', null);
+    user.set('passwordResetExpires', null);
+    user.save(user.changed, { patch: true }).then(function() {
+      req.logIn(user, function(err) {
+        done(err, user.toJSON());
       });
+    });
   });
