0d32c28180acda171886fd6f9fbd325787c48a31
Sahat Yalkabov <sakhat@gmail.com>
2016-03-16 22:22:10 -0400

    Removed isAuthorized function

--- generators/authentication/modules/common/passport-config.js
+++ generators/authentication/modules/common/passport-config.js
@@ -13,25 +13,10 @@ var passport = require('passport');
 //= PASSPORT_GOOGLE_STRATEGY
 //= PASSPORT_TWITTER_STRATEGY
 
-/**
- * Login Required middleware.
- */
+// Login Required middleware.
 exports.isAuthenticated = function(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/login');
-};
-
-/**
- * Authorization Required middleware.
- */
-exports.isAuthorized = function(req, res, next) {
-  var provider = req.path.split('/').slice(-1)[0];
-
-  if (_.find(req.user.tokens, { kind: provider })) {
-    next();
-  } else {
-    res.redirect('/auth/' + provider);
-  }
 };
