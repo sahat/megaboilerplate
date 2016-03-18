8ae1ab73e168dc9d9d1d25288e23b71e23937aae
Sahat Yalkabov <sakhat@gmail.com>
2016-03-17 23:16:36 -0400

    Upgrade to react-router 2.0

--- site/main.js
+++ site/main.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Router from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import ReactDOM from 'react-dom';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
 import routes from './routes';
 
-let history = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={history}>{routes}</Router>,
+  <Router history={browserHistory}>{routes}</Router>,
   document.getElementById('root')
 );
