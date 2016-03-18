3c8e99a91dba37971ceba4558dc52b8fc327cd55
Sahat Yalkabov <sakhat@gmail.com>
2016-03-17 21:50:19 -0400

    Updated React generator: copy components and routes

--- generators/js-framework/generateJsFrameworkReact.js
+++ generators/js-framework/generateJsFrameworkReact.js
@@ -1,5 +1,5 @@
 import { join } from 'path';
-import { copy, mkdirs, addNpmPackage, replaceCode } from '../utils';
+import { copy, cpy, mkdirs, addNpmPackage, replaceCode } from '../utils';
 
 async function generateJsFrameworkReact(params) {
   const build = join(__base, 'build', params.uuid);
@@ -8,28 +8,43 @@ async function generateJsFrameworkReact(params) {
   const mainJs = join(__dirname, 'modules', 'react', 'main.js');
   const reactRequire = join(__dirname, 'modules', 'react', 'react-require.js');
   const reactRoutesRequire = join(__dirname, 'modules', 'react', 'react-routes-require.js');
+  const reactRoutes = join(__dirname, 'modules', 'react', 'routes.js');
   const serverRenderingWithRouting = join(__dirname, 'modules', 'react', 'server-rendering-with-routing.js');
 
   switch (params.framework) {
     case 'express':
-      
-      await mkdirs(join(build, 'app', 'components'));
 
       await addRedux(params);
 
-      // Require react and react-dom packages
+      // Require react, react-router, react-dom packages
       await replaceCode(app, 'REACT_REQUIRE', reactRequire);
-      
-      // Require react routes
       await replaceCode(app, 'REACT_ROUTES_REQUIRE', reactRoutesRequire);
 
       // Add ES6 transpiler
       await replaceCode(app, 'ES6_TRANSPILER', es6Transpiler);
 
-      // Add react server-rendering with react-router middleware
+      // Add server-rendering  middleware
       await replaceCode(join(build, 'app.js'), 'REACT_SERVER_RENDERING', serverRenderingWithRouting);
 
+      // Copy React components
+      await mkdirs(join(build, 'app', 'components'));
+      await cpy([
+        join(__dirname, 'modules', 'react', 'components', 'App.js'),
+        join(__dirname, 'modules', 'react', 'components', 'Home.js'),
+        join(__dirname, 'modules', 'react', 'components', 'Contant.js'),
+        join(__dirname, 'modules', 'react', 'components', 'Header.js'),
+        join(__dirname, 'modules', 'react', 'components', 'Footer.js')
+      ], join(build, 'app', 'components'));
+      await cpy([
+        join(__dirname, 'modules', 'react', 'components', 'Account', 'Login.js'),
+        join(__dirname, 'modules', 'react', 'components', 'Account', 'Signup.js'),
+        join(__dirname, 'modules', 'react', 'components', 'Account', 'Profile.js'),
+        join(__dirname, 'modules', 'react', 'components', 'Account', 'Forgot.js'),
+        join(__dirname, 'modules', 'react', 'components', 'Account', 'Reset.js')
+      ], join(build, 'app', 'components', 'Account'));
+
       await copy(mainJs, join(build, 'app', 'main.js'));
+      await copy(reactRoutes, join(build, 'app', 'routes.js'));
 
       switch (params.templateEngine) {
         case 'jade':
@@ -56,10 +71,6 @@ async function generateJsFrameworkReact(params) {
         default:
           break;
       }
-
-      await addNpmPackage('react', params);
-      await addNpmPackage('react-dom', params);
-
       break;
 
     case 'hapi':
@@ -70,6 +81,9 @@ async function generateJsFrameworkReact(params) {
 
     default:
   }
+
+  await addNpmPackage('react', params);
+  await addNpmPackage('react-dom', params);
 }
 
 async function addRedux(params) {
