52acc64f8e5e536b1182b0d964973671b2352f33
Sahat Yalkabov <sakhat@gmail.com>
2016-03-20 00:43:33 -0400

    Use Redux for header component, show user info if available

--- generators/js-framework/modules/react/components/Header.js
+++ generators/js-framework/modules/react/components/Header.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux'
 
 class Header extends React.Component {
   render() {
+    const { user } = this.props;
+
+    let rightNav;
+
+    if (user) {
+      rightNav = (
+        <ul className="nav navbar-nav navbar-right">
+          <li className="dropdown">
+            <a href="#" data-toggle="dropdown" className="navbar-avatar dropdown-toggle">
+              <img src={user.picture || user.gravatar} /> {user.name || user.email || user.id}
+              <i className="caret"></i>
+            </a>
+            <ul className="dropdown-menu">
+              <li><Link to="/account">My Account</Link></li>
+              <li className="divider"></li>
+              <li><Link to="/logout">Logout</Link></li>
+            </ul>
+          </li>
+        </ul>
+      );
+    } else {
+      rightNav = (
+        <ul className="nav navbar-nav navbar-right">
+          <li><Link to="/login">Log in</Link></li>
+          <li><Link to="/signup">Sign up</Link></li>
+        </ul>
+      );
+    }
+
     return (
       <nav className="navbar navbar-default navbar-static-top">
         <div className="container">
@@ -20,10 +50,7 @@ class Header extends React.Component {
               <li><Link to="/">Home</Link></li>
               <li><Link to="/contact">Contact</Link></li>
             </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li><Link to="/login">Log in</Link></li>
-              <li><Link to="/signup">Sign up</Link></li>
-            </ul>
+            {rightNav}
           </div>
         </div>
       </nav>
@@ -31,4 +58,11 @@ class Header extends React.Component {
   }
 }
 
-export default Header;
+const mapReduxStateToHeaderProps = (state) => {
+  return {
+    token: state.token,
+    user: state.user
+  };
+};
+
+export default connect(mapReduxStateToHeaderProps)(Header);
