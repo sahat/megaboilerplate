17b881533d8c700aa060097715f96e6ca77d7f7a
Sahat Yalkabov <sakhat@gmail.com>
2016-03-20 16:29:51 -0400

    Updated react routes

--- generators/js-framework/modules/react/routes.js
+++ generators/js-framework/modules/react/routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { IndexRoute, Route } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 import Login from './components/Account/Login';
 import Signup from './components/Account/Signup';
 import Profile from './components/Account/Profile';
@@ -10,13 +11,14 @@ import Forgot from './components/Account/Forgot';
 import Reset from './components/Account/Reset';
 
 export default (
-  <Route component={App}>
-    <Route path='/' component={Home}/>
-    <Route path='/contact' component={Contact}/>
-    <Route path='/login' component={Login}/>
-    <Route path='/signup' component={Signup}/>
-    <Route path='/account' component={Profile}/>
-    <Route path='/forgot' component={Forgot}/>
-    <Route path='/reset' component={Reset}/>
+  <Route path ="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="contact" component={Contact} />
+      <Route path="login" component={Login} />
+      <Route path="signup" component={Signup} />
+      <Route path="account" component={Profile} />
+      <Route path="forgot" component={Forgot} />
+      <Route path='reset' component={Reset} />
+      <Route path="*" component={NotFound}/>
   </Route>
 );
