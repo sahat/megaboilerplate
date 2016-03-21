e05fc997054a31e7f2768ee61e9ad7a8108778e0
Sahat Yalkabov <sakhat@gmail.com>
2016-03-21 18:11:31 -0400

    Add redux support for Forgot password component

--- generators/js-framework/modules/react/components/Account/Forgot.js
+++ generators/js-framework/modules/react/components/Account/Forgot.js
@@ -1,17 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { forgotPassword, clearMessages } from '../../actions/auth';
+import Messages from '../Shared/Messages';
 
 class Forgot extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleForgot = this.handleForgot.bind(this);
+    this.state = { email: '' };
+  }
+
+  componentWillMount() {
+    this.props.dispatch(clearMessages());
+  }
+
+  handleChange(event) {
+    const input = event.target;
+    this.setState({ [input.name]: input.value });
+  }
+
+  handleForgot(event) {
+    event.preventDefault();
+    const { email } = this.state;
+    const { dispatch, token } = this.props;
+    dispatch(forgotPassword(email));
+  }
+
   render() {
     return (
       <div className="container">
         <div className="panel">
           <div className="panel-body">
-            <form method="POST">
+            <Messages messages={this.props.messages} />
+            <form onSubmit={this.handleForgot}>
               <legend>Forgot Password</legend>
               <div className="form-group">
                 <p>Enter your email address below and we'll send you password reset instructions.</p>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Email" autoFocus className="form-control"/>
+                <input type="email" name="email" id="email" placeholder="Email" className="form-control" autoFocus value={this.state.email} onChange={this.handleChange}/>
               </div>
               <button type="submit" className="btn btn-success">Reset Password</button>
             </form>
@@ -22,4 +49,10 @@ class Forgot extends React.Component {
   }
 }
 
-export default Forgot;
+const mapReduxStateToForgotProps = (state) => {
+  return {
+    messages: state.messages
+  };
+};
+
+export default connect(mapReduxStateToForgotProps)(Forgot);
