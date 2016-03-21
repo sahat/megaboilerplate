dbbdef90c38f1b2e7ccaec02b587894a7c9d2973
Sahat Yalkabov <sakhat@gmail.com>
2016-03-20 23:26:43 -0400

    Added routes helper function requireAuth and skipIfAuthenticated

--- generators/js-framework/modules/react/routes.js
+++ generators/js-framework/modules/react/routes.js
@@ -10,15 +10,27 @@ import Profile from './components/Account/Profile';
 import Forgot from './components/Account/Forgot';
 import Reset from './components/Account/Reset';
 
-export default (
-  <Route path="/" component={App}>
-    <IndexRoute component={Home}/>
-    <Route path="/contact" component={Contact}/>
-    <Route path="/login" component={Login}/>
-    <Route path="/signup" component={Signup}/>
-    <Route path="/account" component={Profile}/>
-    <Route path="/forgot" component={Forgot}/>
-    <Route path='/reset' component={Reset}/>
-    <Route path="*" component={NotFound}/>
-  </Route>
-);
+export default (store) => {
+  const requireAuth = (nextState, replace) => {
+    if (!store.getState().token) {
+      replace('/login');
+    }
+  };
+  const skipIfAuthenticated = (nextState, replace) => {
+    if (store.getState().token) {
+      replace('/');
+    }
+  };
+  return (
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="/contact" component={Contact}/>
+      <Route path="/login" component={Login} onEnter={skipIfAuthenticated}/>
+      <Route path="/signup" component={Signup} onEnter={skipIfAuthenticated}/>
+      <Route path="/account" component={Profile} onEnter={requireAuth}/>
+      <Route path="/forgot" component={Forgot}/>
+      <Route path='/reset' component={Reset}/>
+      <Route path="*" component={NotFound}/>
+    </Route>
+  );
+}
