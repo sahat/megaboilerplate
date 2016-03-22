e5915133b0634fb530ada787c3b7ca6b8f36f17b
Sahat Yalkabov <sakhat@gmail.com>
2016-03-22 15:37:37 -0400

    Refactored Header react component

--- generators/js-framework/modules/react/components/Header.js
+++ generators/js-framework/modules/react/components/Header.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
 import { connect } from 'react-redux'
+import { logout } from '../actions/auth';
 
 class Header extends React.Component {
+  handleLogout(event) {
+    event.preventDefault();
+    this.props.dispatch(logout());
+  }
+
   render() {
-    const ACTIVE = { borderBottomColor: '#3f51b5' };
-    const { token, user } = this.props;
+    const active = { borderBottomColor: '#3f51b5' };
 
-    let rightNav;
-
-    if (token) {
-      rightNav = (
-        <ul className="nav navbar-nav navbar-right">
-          <li className="dropdown">
-            <a href="#" data-toggle="dropdown" className="navbar-avatar dropdown-toggle">
-              <img src={user.picture || user.gravatar}/> {user.name || user.email || user.id}
-              <i className="caret"></i>
-            </a>
-            <ul className="dropdown-menu">
-              <li><Link to="/account">My Account</Link></li>
-              <li className="divider"></li>
-              <li><Link to="/logout">Logout</Link></li>
-            </ul>
-          </li>
-        </ul>
-      );
-    } else {
-      rightNav = (
-        <ul className="nav navbar-nav navbar-right">
-          <li><Link to="/login" activeStyle={ACTIVE}>Log in</Link></li>
-          <li><Link to="/signup" activeStyle={ACTIVE}>Sign up</Link></li>
-        </ul>
-      );
-    }
+    const rightNav = this.props.token ? (
+      <ul className="nav navbar-nav navbar-right">
+        <li className="dropdown">
+          <a href="#" data-toggle="dropdown" className="navbar-avatar dropdown-toggle">
+            <img src={this.props.user.picture || this.props.user.gravatar}/>
+            {' '}{this.props.user.name || this.props.user.email || this.props.user.id}{' '}
+            <i className="caret"></i>
+          </a>
+          <ul className="dropdown-menu">
+            <li><Link to="/account">My Account</Link></li>
+            <li className="divider"></li>
+            <li><a href="#" onClick={this.handleLogout.bind(this)}>Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    ) : (
+      <ul className="nav navbar-nav navbar-right">
+        <li><Link to="/login" activeStyle={active}>Log in</Link></li>
+        <li><Link to="/signup" activeStyle={active}>Sign up</Link></li>
+      </ul>
+    );
 
     return (
       <nav className="navbar navbar-default navbar-static-top">
@@ -44,12 +44,12 @@ class Header extends React.Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <IndexLink to="/" activeStyle={ACTIVE} className="navbar-brand">Project name</IndexLink>
+            <IndexLink to="/" activeStyle={active} className="navbar-brand">Project name</IndexLink>
           </div>
           <div id="navbar" className="navbar-collapse collapse">
             <ul className="nav navbar-nav">
-              <li><IndexLink to="/" activeStyle={ACTIVE}>Home</IndexLink></li>
-              <li><Link to="/contact" activeStyle={ACTIVE}>Contact</Link></li>
+              <li><IndexLink to="/" activeStyle={active}>Home</IndexLink></li>
+              <li><Link to="/contact" activeStyle={active}>Contact</Link></li>
             </ul>
             {rightNav}
           </div>
@@ -59,11 +59,11 @@ class Header extends React.Component {
   }
 }
 
-const mapReduxStateToHeaderProps = (state) => {
+const mapStateToProps = (state) => {
   return {
-    token: state.token,
-    user: state.user
+    token: state.auth.token,
+    user: state.auth.user
   };
 };
 
-export default connect(mapReduxStateToHeaderProps)(Header);
+export default connect(mapStateToProps)(Header);
