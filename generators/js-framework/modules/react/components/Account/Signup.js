764053050ef1b7798cd16790c62ac596d350091c
Sahat Yalkabov <sakhat@gmail.com>
2016-03-22 15:58:10 -0400

    Converted Signup component to redux

--- generators/js-framework/modules/react/components/Account/Signup.js
+++ generators/js-framework/modules/react/components/Account/Signup.js
@@ -1,33 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux'
+import { signup } from '../../actions/auth';
+import Messages from '../Messages';
 
 class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '', password: '' };
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSignup(event) {
+    event.preventDefault();
+    this.props.dispatch(signup(this.state.name, this.state.email, this.state.password));
+  }
+
   render() {
     return (
       <div className="login-container">
         <div className="panel">
           <div className="panel-body">
-            <form method="POST">
+            <Messages messages={this.props.messages}/>
+            <form onSubmit={this.handleSignup.bind(this)}>
               <legend>Create an account</legend>
               <div className="form-group">
                 <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" placeholder="Name" autoFocus className="form-control"/>
+                <input type="text" name="name" id="name" placeholder="Name" autoFocus className="form-control" value={this.state.name} onChange={this.handleChange.bind(this)}/>
               </div>
               <div className="form-group">
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Email" className="form-control"/>
+                <input type="email" name="email" id="email" placeholder="Email" className="form-control" value={this.state.email} onChange={this.handleChange.bind(this)}/>
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="Password" className="form-control"/>
+                <input type="password" name="password" id="password" placeholder="Password" className="form-control" value={this.state.password} onChange={this.handleChange.bind(this)}/>
               </div>
-              <div className="form-group"><small className="text-muted">By signing up, you agree to the <Link to="/">Terms of Service</Link>.</small></div>
+              <div className="form-group">
+                <small className="text-muted">By signing up, you agree to the <Link to="/">Terms of Service</Link>.</small>
+              </div>
               <button type="submit" className="btn btn-success">Create an account</button>
             </form>
             <div className="hr-title"><abbr>or</abbr></div>
             <div className="text-center">
-              <a href="/auth/facebook" className="btn btn-facebook">Sign in with Facebook</a><span> </span>
-              <a href="/auth/twitter" className="btn btn-twitter">Sign in with Twitter</a><span> </span>
+              <a href="/auth/facebook" className="btn btn-facebook">Sign in with Facebook</a>{' '}
+              <a href="/auth/twitter" className="btn btn-twitter">Sign in with Twitter</a>{' '}
               <a href="/auth/google" className="btn btn-google">Sign in with Google</a></div>
           </div>
         </div>
@@ -39,4 +59,10 @@ class Signup extends React.Component {
   }
 }
 
-export default Signup;
+const mapStateToProps = (state) => {
+  return {
+    messages: state.messages
+  };
+};
+
+export default connect(mapStateToProps)(Signup);
