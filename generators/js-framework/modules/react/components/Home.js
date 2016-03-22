9d53391bff932b886120833d86952bf47ba29fa5
Sahat Yalkabov <sakhat@gmail.com>
2016-03-22 15:04:51 -0400

    Connected home component to redux store

--- generators/js-framework/modules/react/components/Home.js
+++ generators/js-framework/modules/react/components/Home.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import Messages from './Messages';
 
 class Home extends React.Component {
   render() {
     return (
       <div className="container-fluid">
+        <Messages messages={this.props.messages}/>
         <div className="row">
           <div className="col-sm-4">
             <div className="panel">
               <div className="panel-body">
                 <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p><a href="#" role="button" className="btn btn-default">View details</a>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                  mollis euismod. Donec sed odio dui.</p><a href="#" role="button" className="btn btn-default">View
+                details</a>
               </div>
             </div>
           </div>
@@ -17,7 +23,10 @@ class Home extends React.Component {
             <div className="panel">
               <div className="panel-body">
                 <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p><a href="#" role="button" className="btn btn-default">View details</a>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                  mollis euismod. Donec sed odio dui.</p><a href="#" role="button" className="btn btn-default">View
+                details</a>
               </div>
             </div>
           </div>
@@ -25,7 +34,10 @@ class Home extends React.Component {
             <div className="panel">
               <div className="panel-body">
                 <h3>Heading</h3>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p><a href="#" role="button" className="btn btn-default">View details</a>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                  mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                  mollis euismod. Donec sed odio dui.</p><a href="#" role="button" className="btn btn-default">View
+                details</a>
               </div>
             </div>
           </div>
@@ -35,4 +47,10 @@ class Home extends React.Component {
   }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    messages: state.messages
+  };
+};
+
+export default connect(mapStateToProps)(Home);
