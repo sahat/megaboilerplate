5748d36b869f134c86680e88c8e200af8a352014
Sahat Yalkabov <sakhat@gmail.com>
2016-03-22 16:00:31 -0400

    Converted Forgot and Reset components to redux

--- generators/js-framework/modules/react/components/Account/Forgot.js
+++ generators/js-framework/modules/react/components/Account/Forgot.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { forgotPassword, clearMessages } from '../../actions/auth';
-import Messages from '../Shared/Messages';
+import { forgotPassword } from '../../actions/auth';
+import Messages from '../Messages';
 
 class Forgot extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleForgot = this.handleForgot.bind(this);
     this.state = { email: '' };
   }
 
-  componentWillMount() {
-    this.props.dispatch(clearMessages());
-  }
-
   handleChange(event) {
-    const input = event.target;
-    this.setState({ [input.name]: input.value });
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleForgot(event) {
     event.preventDefault();
-    const { email } = this.state;
-    const { dispatch, token } = this.props;
-    dispatch(forgotPassword(email));
+    this.props.dispatch(forgotPassword(this.state.email));
   }
 
   render() {
@@ -33,12 +24,12 @@ class Forgot extends React.Component {
         <div className="panel">
           <div className="panel-body">
             <Messages messages={this.props.messages} />
-            <form onSubmit={this.handleForgot}>
+            <form onSubmit={this.handleForgot.bind(this)}>
               <legend>Forgot Password</legend>
               <div className="form-group">
                 <p>Enter your email address below and we'll send you password reset instructions.</p>
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Email" className="form-control" autoFocus value={this.state.email} onChange={this.handleChange}/>
+                <input type="email" name="email" id="email" placeholder="Email" className="form-control" autoFocus value={this.state.email} onChange={this.handleChange.bind(this)}/>
               </div>
               <button type="submit" className="btn btn-success">Reset Password</button>
             </form>
@@ -49,10 +40,10 @@ class Forgot extends React.Component {
   }
 }
 
-const mapReduxStateToForgotProps = (state) => {
+const mapStateToProps = (state) => {
   return {
     messages: state.messages
   };
 };
 
-export default connect(mapReduxStateToForgotProps)(Forgot);
+export default connect(mapStateToProps)(Forgot);
