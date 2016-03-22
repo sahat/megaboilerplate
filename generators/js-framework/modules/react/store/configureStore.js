efb7570a4237f5d39dbdef352f6cac852a3e50b7
Sahat Yalkabov <sakhat@gmail.com>
2016-03-22 13:52:04 -0400

    Added configureStore helper function for creating redux store

---
+++ generators/js-framework/modules/react/store/configureStore.js
@@ -0,0 +1,16 @@
+import {applyMiddleware, compose, createStore} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from '../reducers'
+
+export default function configureStore(initialState) {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(thunk),
+      typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
+    )
+  );
+
+  return store;
+}
