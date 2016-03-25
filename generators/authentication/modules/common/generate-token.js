2e42a6a3371f3b63899a4195c4d1edc3a2522cc0
Sahat Yalkabov <sakhat@gmail.com>
2016-03-25 19:28:07 -0400

    GenerateToken now includes most user fields

--- generators/authentication/modules/common/generate-token.js
+++ generators/authentication/modules/common/generate-token.js
@@ -1,8 +1,19 @@
 function generateToken(user) {
   var payload = {
-    sub: user.id,
+    iss: 'my.domain.com',
+    iat: moment().unix(),
     exp: moment().add(1, 'hour').unix(),
-    iat: moment().unix()
+    sub: user.id,
+    name: user.name,
+    email: user.email,
+    gender: user.gender,
+    location: user.gender,
+    website: user.website,
+    picture: user.picture,
+    gravatar: user.gravatar,
+    facebook: user.facebook,
+    twitter: user.twitter,
+    google: user.google
   };
   return jwt.sign(payload, process.env.TOKEN_SECRET);
 }
