68dea48bd0d92fbcf8f3b20bddc04f1cd5b89caa
Sahat Yalkabov <sakhat@gmail.com>
2016-03-25 20:39:58 -0400

    Send valid json message back when used is unauthorized

--- generators/authentication/modules/common/ensure-authenticated-jwt.js
+++ generators/authentication/modules/common/ensure-authenticated-jwt.js
@@ -3,6 +3,6 @@ exports.ensureAuthenticated = function(req, res, next) {
   if (req.isAuthenticated()) {
     next();
   } else {
-    res.sendStatus(401);
+    res.status(401).send({ msg: 'Unauthorized' });
   }
 };
