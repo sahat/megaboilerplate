968c29becd64666987792e47d594fc043f9b1ae1
Sahat Yalkabov <sakhat@gmail.com>
2016-03-26 14:37:45 -0400

    Split profile update success response into module

--- generators/authentication/modules/controllers/mongodb/user-account-put.js
+++ generators/authentication/modules/controllers/mongodb/user-account-put.js
@@ -9,13 +9,6 @@ User.findById(req.user.id, function(err, user) {
     user.website = req.body.website;
   }
   user.save(function(err) {
-    if ('password' in req.body) {
-      req.flash('success', { msg: 'Your password has been changed.' });
-    } else if (err && err.code === 11000) {
-      req.flash('error', { msg: 'The email address you have entered is already associated with an account.' });
-    } else {
-      req.flash('success', { msg: 'Your profile information has been updated.' });
-    }
-    res.redirect('/account');
+    //= PROFILE_UPDATE_SUCCESS
   });
 });
