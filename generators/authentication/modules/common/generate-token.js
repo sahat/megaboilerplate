87dc9463b083f2a1a5b28397445a87c8db5c98ac
Sahat Yalkabov <sakhat@gmail.com>
2016-03-25 20:44:58 -0400

    Removed user fields from generateToken

--- generators/authentication/modules/common/generate-token.js
+++ generators/authentication/modules/common/generate-token.js
@@ -1,19 +1,9 @@
 function generateToken(user) {
   var payload = {
     iss: 'my.domain.com',
-    iat: moment().unix(),
-    exp: moment().add(1, 'hour').unix(),
     sub: user.id,
-    name: user.name,
-    email: user.email,
-    gender: user.gender,
-    location: user.gender,
-    website: user.website,
-    picture: user.picture,
-    gravatar: user.gravatar,
-    facebook: user.facebook,
-    twitter: user.twitter,
-    google: user.google
+    iat: moment().unix(),
+    exp: moment().add(7, 'days').unix()
   };
   return jwt.sign(payload, process.env.TOKEN_SECRET);
 }
