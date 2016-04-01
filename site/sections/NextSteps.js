9acc10616128cbd3cfc707efccd7131c0658a920
Sahat Yalkabov <sakhat@gmail.com>
2016-03-31 21:08:26 -0400

    Dynamic text for getting started column

--- site/sections/NextSteps.js
+++ site/sections/NextSteps.js
@@ -3,15 +3,39 @@ import { VelocityComponent, VelocityTransitionGroup } from 'velocity-react';
 
 const CHECK_SVG = (
   <svg xmlns="http://www.w3.org/2000/svg" version="1"  width="18px" height="18px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" style={{ verticalAlign: 'bottom' }}>
-    <path  d="M 12 2 C 6.5 2 2 6.5 2 12 C 2 17.5 6.5 22 12 22 C 17.2 22 21.40625 18.1 21.90625 13 L 19.90625 13 C 19.40625 16.9 16 20 12 20 C 7.6 20 4 16.4 4 12 C 4 7.6 7.6 4 12 4 C 13.4 4 14.70625 4.4 15.90625 5 L 17.40625 3.5 C 15.80625 2.6 14 2 12 2 z M 20.5 4.875 L 11.1875 13.78125 L 7.5 10.09375 L 6.09375 11.5 L 10.5 15.90625 L 11.1875 16.59375 L 11.90625 15.90625 L 21.90625 6.3125 L 20.5 4.875 z" color="#000" overflow="visible" enable-background="accumulate" font-family="Bitstream Vera Sans"></path>
+    <path  d="M 12 2 C 6.5 2 2 6.5 2 12 C 2 17.5 6.5 22 12 22 C 17.2 22 21.40625 18.1 21.90625 13 L 19.90625 13 C 19.40625 16.9 16 20 12 20 C 7.6 20 4 16.4 4 12 C 4 7.6 7.6 4 12 4 C 13.4 4 14.70625 4.4 15.90625 5 L 17.40625 3.5 C 15.80625 2.6 14 2 12 2 z M 20.5 4.875 L 11.1875 13.78125 L 7.5 10.09375 L 6.09375 11.5 L 10.5 15.90625 L 11.1875 16.59375 L 11.90625 15.90625 L 21.90625 6.3125 L 20.5 4.875 z" color="#000" overflow="visible" enable-background="accumulate"></path>
   </svg>
 );
 
 const NextSteps = (props) => {
+  let gettingStarted = {};
   let description;
 
-  switch (props.database) {
-    case 'mongodb':
+  let nameMap = {
+    express: 'Express',
+    meteor: 'Meteor',
+    mongodb: 'MongoDB',
+    mysql: 'MySQL',
+    postgresql: 'PostgreSQL',
+    sqlite: 'SQLite',
+  };
+
+  let urlMap = {
+    express: '-express',
+    meteor: '-meteor',
+    mongodb: '-mongodb',
+    mysql: '-mysql',
+    postgresql: '-postgresql',
+    sqlite: '-sqlite',
+  };
+
+  if (props.framework === 'express') {
+    gettingStarted.framework = 'Express';
+    gettingStarted.frameworkUrl = '-express';
+  }
+
+  switch (props.platform) {
+    case 'node':
       description = (
         <div>
           <strong><a href="http://mongodb.org/" target="_blank">MongoDB</a></strong> — Document-oriented, general purpose NoSQL database.
@@ -43,6 +67,18 @@ const NextSteps = (props) => {
       description = <div className="placeholder"> </div>;
   }
 
+  let gettingStartedText = `See how to <a href="https://github.com/sahat/boilerplate#${urlMap[props.framework]}" target="_blank">get started</a> with <strong>${nameMap[props.framework]}</strong>`;
+
+  if (props.database && props.database !== 'none') {
+    gettingStartedText += ` and <a href="https://github.com/sahat/boilerplate#${urlMap[props.database]}" target="_blank">setup</a> <strong>${nameMap[props.database]}</strong> database.`
+  } else {
+    gettingStartedText += `.`;
+  }
+
+  if (props.authentication && props.authentication.size) {
+    gettingStartedText += ` Then go to <a href="https://github.com/sahat/boilerplate/README.md#obtaining-api-keys">Obtaining API Keys</a> to learn how to generate your own OAuth keys.`;
+  }
+
   return (
     <div className="panel zoomInBackwards">
       <div className="panel-body">
@@ -57,9 +93,7 @@ const NextSteps = (props) => {
             <div className="col-xs-4" style={{ opacity: 0 }}>
               <div className="text-center">
                 <img className="next-steps-icon" src="/img/svg/rocket.svg" alt="Getting Started"/>
-                <p>
-                  See how to <a href="/introduction/build-tools.html">get started</a> with <strong>Express</strong> and <a href="#">setup</a> a <strong>MySQL</strong> database.
-                </p>
+                <p dangerouslySetInnerHTML={{ __html: gettingStartedText }} />
               </div>
           </div>
           </VelocityComponent>
