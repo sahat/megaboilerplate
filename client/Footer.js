f53ff336bcdeea22723579d325aef3440c7e586a
Sahat Yalkabov <sakhat@gmail.com>
2016-04-02 13:09:36 -0400

    Updated footer

--- client/Footer.js
+++ client/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Router, Route, Link } from 'react-router'
 
 const Footer = (props) => {
   return (
     <footer className="container">
-      © 2016 <a href="http://sahatyalkabov.com">Sahat Yalkabov</a>. View <a href="https://github.com/sahat/boilerplate">Source Code</a>.
+      <div>© 2016 <a href="http://sahatyalkabov.com">Sahat Yalkabov</a>. MIT Licensed. View <a href="https://github.com/sahat/boilerplate">Source Code</a>.</div>
+      <div><fa className="fa fa-heart text-danger"></fa> Open Source</div>
     </footer>
   );
 };
