e4a7d49e72108f8f721980ffa1e3ed2e1bcdd2b8
Sahat Yalkabov <sakhat@gmail.com>
2016-04-03 19:54:14 -0400

    Fixed user assignment in isAuthenticated middleware

--- generators/authentication/modules/common/is-authenticated-jwt.js
+++ generators/authentication/modules/common/is-authenticated-jwt.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
   if (req.isAuthenticated()) {
     var payload = req.isAuthenticated();
     User.findById(payload.sub, function(err, user) {
-      req.user = user.user;
+      req.user = user;
       next();
     });
   } else {
