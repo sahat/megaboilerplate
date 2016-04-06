ee32a8b1253e053edd7f14e6c27f02f7f2da71cc
Sahat Yalkabov <sakhat@gmail.com>
2016-04-05 20:25:59 -0400

    Tokenized css classes in contact component

--- generators/css-framework/modules/class-maps.js
+++ generators/css-framework/modules/class-maps.js
@@ -4,12 +4,15 @@ export const noneClassMap = {
   row: '',
   col4: '',
   fluidContainer: '',
+  container: '',
 
   // Buttons
   defaultButton: '',
 
   // Components
   panel: '',
+  panelHeading: 'panel-heading',
+  panelTitle: 'panel-title',
   panelBody: ''
 };
 
@@ -17,14 +20,26 @@ export const noneClassMap = {
 export const bootstrapClassMap = Object.assign({}, noneClassMap, {
   // Grid
   row: 'row',
+  col2: 'col-sm-2',
   col4: 'col-sm-4',
+  col8: 'col-sm-8',
+  col2OffsetCol8: "col-sm-offset-2 col-sm-8",
   fluidContainer: 'container-fluid',
+  container: 'container',
 
   // Buttons
-  defaultButton: 'button default',
+  defaultButton: 'btn btn-default',
+  successButton: 'btn btn-success',
+  
+  // Forms
+  horizontalForm: 'form-horizontal',
+  formGroup: 'form-group',
+  formInput: 'form-control',
 
   // Components
   panel: 'panel',
+  panelHeading: 'panel-heading',
+  panelTitle: 'panel-title',
   panelBody: 'panel-body'
 });
 
--- generators/js-framework/modules/react/components/Contact.js
+++ generators/js-framework/modules/react/components/Contact.js
@@ -20,35 +20,35 @@ class Contact extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <div className="panel">
-          <div className="panel-heading">
-            <h3 className="panel-title">Contact Form</h3>
+      <div className="<%= container %>">
+        <div className="<%= panel %>">
+          <div className="<%= panelHeading %>">
+            <h3 className="<%= panelTitle %>">Contact Form</h3>
           </div>
-          <div className="panel-body">
+          <div className="<%= panelBody %>">
             <Messages messages={this.props.messages}/>
-            <form onSubmit={this.handleSubmit.bind(this)} className="form-horizontal">
-              <div className="form-group">
-                <label htmlFor="name" className="col-sm-2">Name</label>
-                <div className="col-sm-8">
-                  <input type="text" name="name" id="name" autoFocus className="form-control" value={this.state.name} onChange={this.handleChange.bind(this)}/>
+            <form onSubmit={this.handleSubmit.bind(this)} className="<%= horizontalForm %>">
+              <div className="<%= formGroup %>">
+                <label htmlFor="name" className="<%= col2 %>">Name</label>
+                <div className="<%= col8 %>">
+                  <input type="text" name="name" id="name" className="<%= formInput %>" value={this.state.name} onChange={this.handleChange.bind(this)} autoFocus/>
                 </div>
               </div>
-              <div className="form-group">
-                <label htmlFor="email" className="col-sm-2">Email</label>
-                <div className="col-sm-8">
-                  <input type="email" name="email" id="email" className="form-control" value={this.state.email} onChange={this.handleChange.bind(this)}/>
+              <div className="<%= formGroup %>">
+                <label htmlFor="email" className="<%= col2 %>">Email</label>
+                <div className="<%= col8 %>">
+                  <input type="email" name="email" id="email" className="<%= formInput %>" value={this.state.email} onChange={this.handleChange.bind(this)}/>
                 </div>
               </div>
-              <div className="form-group">
-                <label htmlFor="message" className="col-sm-2">Body</label>
-                <div className="col-sm-8">
-                  <textarea name="message" id="message" rows="7" className="form-control" value={this.state.message} onChange={this.handleChange.bind(this)}></textarea>
+              <div className="<%= formGroup %>">
+                <label htmlFor="message" className="<%= col2 %>">Body</label>
+                <div className="<%= col8 %>">
+                  <textarea name="message" id="message" rows="7" className="<%= formInput %>" value={this.state.message} onChange={this.handleChange.bind(this)}></textarea>
                 </div>
               </div>
-              <div className="form-group">
-                <div className="col-sm-offset-2 col-sm-8">
-                  <button type="submit" className="btn btn-success">Send</button>
+              <div className="<%= formGroup %>">
+                <div className="<%= col2OffsetCol8 %>">
+                  <button type="submit" className="<%= successButton %>">Send</button>
                 </div>
               </div>
             </form>
