6ca13a274b68117b1b1c62b82f6235cc13b07e58
Sahat Yalkabov <sakhat@gmail.com>
2016-04-05 20:00:38 -0400

    Tokenized css classes in react home component

--- generators/css-framework/modules/class-maps.js
+++ generators/css-framework/modules/class-maps.js
@@ -1,14 +1,44 @@
+/**
+ * No CSS Framework
+ */
 export const noneClassMap = {
   defaultButton: 'button default'
 };
 
+/**
+ * Bootstrap
+ */
 export const bootstrapClassMap = {
-  defaultButton: 'btn btn-default'
-};
+  // Grid
+  row: 'row',
+  col4: 'col-sm-4',
+  fluidContainer: 'container-fluid',
 
-export const foundationClassMap = {
   // Buttons
-  defaultButton: 'button default'
+  defaultButton: 'button default',
+
+  // Components
+  panel: 'panel',
+  panelBody: 'panel-body'
 };
 
+/**
+ * Foundation
+ */
+export const foundationClassMap = {
+  // Grid
+  // row: 'row',
+  // col4: 'col-sm-4',
+  //
+  // // Buttons
+  // defaultButton: 'button default',
+  //
+  // // Components
+  // panel: 'panel',
+  // panelBody: 'panel-body'
+};
+
+/**
+ * Bourbon Neat
+ */
 export const bourbonNeatClassMap = {};
--- generators/js-framework/modules/react/components/Home.js
+++ generators/js-framework/modules/react/components/Home.js
@@ -5,12 +5,12 @@ import Messages from './Messages';
 class Home extends React.Component {
   render() {
     return (
-      <div className="container-fluid">
+      <div className="<%= fluidContainer %>">
         <Messages messages={this.props.messages}/>
-        <div className="row">
-          <div className="col-sm-4">
-            <div className="panel">
-              <div className="panel-body">
+        <div className="<%= row %>">
+          <div className="<%= col4 %>">
+            <div className="<%= panel %>">
+              <div className="<%= panelBody %>">
                 <h3>Heading</h3>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
                   mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
@@ -19,25 +19,25 @@ class Home extends React.Component {
               </div>
             </div>
           </div>
-          <div className="col-sm-4">
-            <div className="panel">
-              <div className="panel-body">
+          <div className="<%= col4 %>">
+            <div className="<%= panel %>">
+              <div className="<%= panelBody %>">
                 <h3>Heading</h3>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
                   mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
                   mollis euismod. Donec sed odio dui.</p>
-                <a href="#" role="button" className="btn btn-default">View details</a>
+                <a href="#" role="button" className="<%= defaultButton %>">View details</a>
               </div>
             </div>
           </div>
-          <div className="col-sm-4">
-            <div className="panel">
-              <div className="panel-body">
+          <div className="<%= col4 %>">
+            <div className="<%= panel %>">
+              <div className="<%= panelBody %>">
                 <h3>Heading</h3>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
                   mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
                   mollis euismod. Donec sed odio dui.</p>
-                <a href="#" role="button" className="btn btn-default">View details</a>
+                <a href="#" role="button" className="<%= defaultButton %>">View details</a>
               </div>
             </div>
           </div>
