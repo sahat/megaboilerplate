59b75e8910b9cd3362863eac29d4ca9c3308d885
Sahat Yalkabov <sakhat@gmail.com>
2016-04-26 01:44:48 -0400

    Dynamically generate google link/unlink in angularjs profile template

--- generators/authentication/generateGoogleAuthenticationExpress.js
+++ generators/authentication/generateGoogleAuthenticationExpress.js
@@ -102,6 +102,20 @@ async function generateGoogleAuthenticationExpress(params) {
     await replaceCode(profileComponent, 'GOOGLE_LINK', googleLinkRender);
     await replaceCode(profileComponent, 'GOOGLE_LINK_REFERENCE', googleLinkReference);
   }
+
+  if (params.jsFramework === 'angularjs') {
+    const angularjsModules = join(__base, 'generators', 'js-framework', 'modules', 'angularjs');
+    // const oauthAction = join(build, 'app', 'actions', 'oauth.js');
+    // const facebookLoginAction = join(reactModules, 'actions', 'oauth', 'facebook.js');
+    // const facebookLinkAction = join(reactModules, 'actions', 'oauth', 'facebook-link.js');
+    // await replaceCode(oauthAction, 'FACEBOOK_LOGIN_ACTION', facebookLoginAction);
+    // await replaceCode(oauthAction, 'FACEBOOK_LINK_ACTION', facebookLinkAction);
+
+    // Add link/unlink button on profile page
+    const profileTemplate = join(build, 'app', 'views', 'profile.html');
+    const googleLink = join(angularjsModules, 'views', 'profile', `google-link-${params.cssFramework}.js`);
+    await replaceCode(profileTemplate, 'GOOGLE_LINK', googleLink);
+  }
 }
 
 
