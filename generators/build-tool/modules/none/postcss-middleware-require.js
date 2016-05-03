117c048aec4a4274ed4f7265e9a0c76d97491a68
Sahat Yalkabov <sakhat@gmail.com>
2016-05-03 13:56:01 -0400

    Added postcss plugins for postcss-middleware

--- generators/build-tool/modules/none/postcss-middleware-require.js
+++ generators/build-tool/modules/none/postcss-middleware-require.js
@@ -1,1 +1,4 @@
-var postcssMiddleware = require('postcss-middleware');
+var postcss = require('postcss-middleware');
+var cssnext = require('postcss-cssnext');
+var atImport = require('postcss-import');
+var autoprefixer = require('autoprefixer');
