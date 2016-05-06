b675129813c731c379ffeca128ad18ae9be21368
Sahat Yalkabov <sakhat@gmail.com>
2016-05-06 00:01:42 -0400

    Mongoose connect error message

--- generators/database/modules/mongodb/mongoose-connect.js
+++ generators/database/modules/mongodb/mongoose-connect.js
@@ -1,2 +1,6 @@
 
 mongoose.connect(process.env.MONGODB);
+mongoose.connection.on('error', function() {
+  console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
+  process.exit(1);
+});
