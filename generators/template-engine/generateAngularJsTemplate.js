822384dec509ace7841d4f66be51bf92128997b2
Sahat Yalkabov <sakhat@gmail.com>
2016-05-16 16:28:27 -0400

    Generate inline angularjs scripts for none build tool

--- generators/template-engine/generateAngularJsTemplate.js
+++ generators/template-engine/generateAngularJsTemplate.js
@@ -2,11 +2,12 @@ import { join } from 'path';
 import { copy, replaceCode } from '../utils';
 
 async function generateAngularJsTemplate(params) {
+  const build = join(__base, 'build', params.uuid);
   switch (params.framework) {
     case 'express':
       // Copy index.html layout to "app" directory
       const indexHtml = join(__dirname, 'modules', 'angularjs', 'index.html');
-      await copy(indexHtml, join(__base, 'build', params.uuid, 'app', 'index.html'));
+      await copy(indexHtml, join(build, 'app', 'index.html'));
 
       // Add/remove features to the newly generated layout file above
       const socketIoImport = join(__dirname, 'modules', 'html-common', 'socketio-import.html');
@@ -15,6 +16,13 @@ async function generateAngularJsTemplate(params) {
       if (params.frameworkOptions.includes('socketio')) {
         await replaceCode(indexHtml, 'SOCKETIO_IMPORT', socketIoImport, { indentLevel: 1 });
       }
+
+      if (params.buildTool === 'none') {
+        await replaceCode(build, 'ANGULARJS_APP_IMPORT', join(__dirname, 'modules', 'angularjs', 'no-build-scripts.html'));
+      } else {
+        await replaceCode(build, 'ANGULARJS_APP_IMPORT', join(__dirname, 'modules', 'angularjs', 'build-scripts.html'));
+      }
+
 
       break;
 
