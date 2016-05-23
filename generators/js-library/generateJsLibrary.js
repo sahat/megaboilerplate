5a4f8de5d235bad43cbe92adf995c93794ad8bad
Sahat Yalkabov <sakhat@gmail.com>
2016-05-23 03:31:49 -0400

    In memory js library generator

--- generators/js-library/generateJsLibrary.js
+++ generators/js-library/generateJsLibrary.js
@@ -1,12 +1,11 @@
 import { join } from 'path';
-import { cpy, copy, mkdirs, replaceCode, templateReplace, addNpmScript, addNpmPackage } from '../utils';
+import { slugify, getModule, replaceCodeMemory, templateReplaceMemory, addNpmScriptMemory, addNpmPackageMemory } from '../utils';
 
 async function generateJsLibrary(params) {
-  const build = join(__base, 'build', params.uuid);
-  const author = params.jsLibraryAuthor;
+  const author = params.jsLibraryAuthor || 'Author name';
   const libraryName = slugify(params.jsLibraryName || params.appName);
   const description = 'Library Description';
-  const username = params.jsLibraryGithubUsername;
+  const username = params.jsLibraryGithubUsername || 'username';
   const options = params.jsLibraryOptions;
   const licenseMap = {
     mit: 'MIT',
@@ -14,46 +13,42 @@ async function generateJsLibrary(params) {
     gplv3: 'GPLv3'
   };
 
-  await mkdirs(join(build, 'examples'));
-  await mkdirs(join(build, 'src'));
-  await mkdirs(join(build, 'test'));
+  params.build = {
+    examples: {
+      'browser.html': await getModule('js-library/examples/browser.html'),
+      'node.js': await getModule('js-library/examples/node.js')
+    },
+    src: {
+      'index.js': await getModule('js-library/src/index.js')
+    },
+    test: {
+      'index.test.js': await getModule('js-library/test/index.test.js')
+    },
+    '.babelrc': await getModule('js-library/.babelrc'),
+    '.gitignore': await getModule('js-library/.gitignore'),
+    '.npmignore': await getModule('js-library/.npmignore'),
+    'CHANGELOG.md': await getModule('js-library/CHANGELOG.md'),
+    'package.json': await getModule('js-library/package.json'),
+    'README.md': await getModule('js-library/README.md'),
+    'webpack.config.js': await getModule('js-library/webpack.config.js')
+  };
 
-  await cpy([
-    join(__dirname, 'modules', 'examples', 'browser.html'),
-    join(__dirname, 'modules', 'examples', 'node.js')
-  ], join(build, 'examples'));
+  params.build.examples['browser.html'] = templateReplaceMemory(params.build.examples['browser.html'], {
+    name: libraryName
+  });
 
-  await templateReplace(join(build, 'examples', 'browser.html'), { name: libraryName });
-
-  await cpy([
-    join(__dirname, 'modules', 'src', 'index.js'),
-    join(__dirname, 'modules', 'src', 'another.js')
-  ], join(build, 'src'));
-
-  await cpy([join(__dirname, 'modules', 'test', 'index.test.js')], join(build, 'test'));
-
-  await cpy([
-    join(__dirname, 'modules', '.babelrc'),
-    join(__dirname, 'modules', '.gitignore'),
-    join(__dirname, 'modules', '.npmignore'),
-    join(__dirname, 'modules', 'CHANGELOG.md'),
-    join(__dirname, 'modules', 'package.json'),
-    join(__dirname, 'modules', 'README.md'),
-    join(__dirname, 'modules', 'webpack.config.js')
-  ], build);
-
-  await templateReplace(join(build, 'CHANGELOG.md'), {
+  params.build['CHANGELOG.md'] = templateReplaceMemory(params.build['CHANGELOG.md'], {
     user: username,
     repo: libraryName
   });
 
-  await templateReplace(join(build, 'README.md'), {
+  params.build['README.md'] = templateReplaceMemory(params.build['README.md'], {
     name: libraryName,
     user: username,
     repo: libraryName
   });
 
-  await templateReplace(join(build, 'package.json'), {
+  params.build['package.json'] = templateReplaceMemory(params.build['package.json'], {
     author: author,
     name: libraryName,
     description: description,
@@ -63,57 +58,46 @@ async function generateJsLibrary(params) {
   });
 
   if (options.includes('eslint')) {
-    await cpy([
-      join(__dirname, 'modules', '.eslintrc'),
-      join(__dirname, 'modules', '.eslintignore')
-    ], build);
-    await addNpmPackage('babel-eslint', params, true);
-    await addNpmPackage('eslint', params, true);
-    await addNpmScript('lint', 'eslint src test examples', params);
+    params.build['.eslintrc'] = await getModule('js-library/.eslintrc');
+    params.build['.eslintignore'] = await getModule('js-library/.eslintignore');
+    
+    await addNpmPackageMemory('babel-eslint', params, true);
+    await addNpmPackageMemory('eslint', params, true);
+    await addNpmScriptMemory('lint', 'eslint src test examples', params);
   }
 
   if (options.includes('travis')) {
-    await cpy([join(__dirname, 'modules', '.travis.yml')], build);
+    params.build['.travis.yml'] = await getModule('js-library/.travis.yml');
   }
 
   if (options.includes('coverage')) {
-    await cpy([join(__dirname, 'modules', '.istanbul.yml')], build);
-    await addNpmPackage('isparta', params, true);
-    await addNpmScript('test:cov', 'babel-node ./node_modules/isparta/bin/isparta cover ./node_modules/mocha/bin/_mocha -- --recursive', params);
+    params.build['.istanbul.yml'] = await getModule('js-library/.istanbul.yml');
+    await addNpmPackageMemory('isparta', params, true);
+    await addNpmScriptMemory('test:cov', 'babel-node ./node_modules/isparta/bin/isparta cover ./node_modules/mocha/bin/_mocha -- --recursive', params);
   }
 
-
   if (options.includes('badges')) {
-    await replaceCode(join(build, 'README.md'), 'BADGES', join(__dirname, 'modules', 'badges.md'));
+    params.build['README.md'] = await replaceCodeMemory(params.build['README.md'], 'BADGES', await getModule('js-library/badges.md'));
   }
 
   switch (params.jsLibraryLicense) {
     case 'mit':
-      await copy(join(__dirname, 'modules', 'license', 'mit'), join(build, 'LICENSE'));
-      await templateReplace(join(build, 'LICENSE'), { author: author });
+      params.build['LICENSE'] = await getModule('js-library/license/mit');
+      params.build['LICENSE'] = templateReplaceMemory(params.build['LICENSE'], { author: author });
       break;
     case 'apache':
-      await copy(join(__dirname, 'modules', 'license', 'apache'), join(build, 'LICENSE'));
-      await templateReplace(join(build, 'LICENSE'), { author: author });
+      params.build['LICENSE'] = await getModule('js-library/license/apache');
+      params.build['LICENSE'] = templateReplaceMemory(params.build['LICENSE'], { author: author });
       break;
     case 'gplv3':
-      await copy(join(__dirname, 'modules', 'license', 'gplv3'), join(build, 'LICENSE'));
-      await templateReplace(join(build, 'LICENSE'), {
+      params.build['LICENSE'] = await getModule('js-library/license/gplv3');
+      params.build['LICENSE'] = templateReplaceMemory(params.build['LICENSE'], {
         author: author,
         description: description,
         name: libraryName
       });
       break;
     default:
-  }
-
-  function slugify(text) {
-    return text.toString().toLowerCase()
-      .replace(/\s+/g, '-')           // Replace spaces with -
-      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-      .replace(/^-+/, '')             // Trim - from start of text
-      .replace(/-+$/, '');            // Trim - from end of text
   }
 }
 
