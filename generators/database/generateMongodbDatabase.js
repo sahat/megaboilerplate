4fc225c5ad25fea455c5db1ec9d701d6db482716
Sahat Yalkabov <sakhat@gmail.com>
2016-05-28 16:37:17 -0400

    MongoDB and SQL in-memory generator

--- generators/database/generateMongodbDatabase.js
+++ generators/database/generateMongodbDatabase.js
@@ -1,34 +1,16 @@
-import { join } from 'path';
-import { addEnv, replaceCode, mkdirs, addNpmPackage } from '../utils';
+import { set } from 'lodash';
+import { getModule, replaceCodeMemory, addEnvMemory, addNpmPackageMemory } from '../utils';
 
-async function generateMongodbDatabase(params) {
+export default async function generateMongodbDatabase(params) {
   switch (params.framework) {
     case 'express':
-      const server = join(__base, 'build', params.uuid, 'server.js');
-      const mongooseRequire = join(__dirname, 'modules', 'mongodb', 'mongoose-require.js');
-      const mongooseConnect = join(__dirname, 'modules', 'mongodb', 'mongoose-connect.js');
-
-      // Add require('mongoose')
-      await replaceCode(server, 'DATABASE_REQUIRE', mongooseRequire);
-
-      // Add mongoose.connect()
-      await replaceCode(server, 'DATABASE_CONNECTION', mongooseConnect);
-
-      // Add MONGODB environment variable
-      await addEnv(params, { MONGODB: 'localhost' });
-
-      // Add mongoose to package.json
-      await addNpmPackage('mongoose', params);
-
-      // Create models/ directory
-      await mkdirs(join(__base, 'build', params.uuid, 'models'));
-      break;
-    case 'hapi':
+      await replaceCodeMemory(params, 'server.js', 'DATABASE_REQUIRE', await getModule('database/mongodb/mongoose-require.js'));
+      await replaceCodeMemory(params, 'server.js', 'DATABASE_CONNECTION', await getModule('database/mongodb/mongoose-connect.js'));
+      await addEnvMemory(params, { MONGODB: 'localhost' });
+      await addNpmPackageMemory('mongoose', params);
       break;
     case 'meteor':
       break;
     default:
   }
 }
-
-export default generateMongodbDatabase;
