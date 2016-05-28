1ce1b59d5c6dec8494c0c84ed0b85257b3ac398a
Sahat Yalkabov <sakhat@gmail.com>
2016-05-28 14:51:55 -0400

    mocha testing in-memory generator

--- generators/testing/generateTestingMocha.js
+++ generators/testing/generateTestingMocha.js
@@ -1,68 +1,53 @@
-import { join } from 'path';
-import { cpy, copy, addNpmPackage, addNpmScript } from '../utils';
+import { set } from 'lodash';
+import { getModule, addNpmScriptMemory, addNpmPackageMemory } from '../utils';
 
-async function generateTestingMocha(params) {
-  const build = join(__base, 'build', params.uuid);
-
+export default async function generateTestingMocha(params) {
   switch (params.framework) {
     case 'express':
-      await addNpmPackage('chai', params, true);
-      await addNpmPackage('mocha', params, true);
-      await addNpmPackage('sinon', params, true);
-      await addNpmPackage('sinon-chai', params, true);
-      await addNpmPackage('supertest', params, true);
+      await addNpmPackageMemory('chai', params, true);
+      await addNpmPackageMemory('mocha', params, true);
+      await addNpmPackageMemory('sinon', params, true);
+      await addNpmPackageMemory('sinon-chai', params, true);
+      await addNpmPackageMemory('supertest', params, true);
 
       if (params.jsFramework) {
         // Server-side tests
-        await copy(join(__dirname, 'modules', 'mocha', 'app.test-json.js'), join(build, 'test', 'server', 'app.test.js'));
+        set(params, ['build', 'test', 'server', 'app.test.js'], await getModule('testing/mocha/app.test-json.js'));
 
         // Client-side tests
         switch (params.jsFramework) {
           case 'react':
-            const reactTests = join(__dirname, 'modules', 'mocha', 'react');
-
-            // Tests for Redux actions
-            await cpy([
-              join(reactTests, 'actions', 'contact.test.js')
-            ], join(build, 'test', 'client', 'actions'));
+            set(params, ['build', 'test', 'client', 'components', 'Home.test.js'], await getModule('testing/mocha/react/components/Home.test.js'));
+            set(params, ['build', 'test', 'client', 'actions', 'contact.test.js'], await getModule('testing/mocha/react/actions/contact.test.js'));
 
             if (params.authentication.length) {
-              await cpy([
-                join(reactTests, 'actions', 'auth.test.js')
-              ], join(build, 'test', 'client', 'actions'));
+              set(params, ['build', 'test', 'client', 'actions', 'auth.test.js'], await getModule('testing/mocha/react/actions/auth.test.js'));
             }
 
-            // Tests for React components
-            await cpy([
-              join(reactTests, 'components', 'Home.test.js')
-            ], join(build, 'test', 'client', 'components'));
+            addNpmPackageMemory('babel-register', params, true);
+            addNpmPackageMemory('babel-plugin-rewire', params, true);
+            addNpmPackageMemory('redux-mock-store', params, true);
+            addNpmPackageMemory('fetch-mock', params, true);
+            addNpmPackageMemory('enzyme', params, true);
+            addNpmPackageMemory('react-addons-test-utils', params, true);
 
-            // Add dependencies
-            addNpmPackage('babel-register', params, true);
-            addNpmPackage('babel-plugin-rewire', params, true);
-            addNpmPackage('redux-mock-store', params, true);
-            addNpmPackage('fetch-mock', params, true);
-            addNpmPackage('enzyme', params, true);
-            addNpmPackage('react-addons-test-utils', params, true);
-
-            await addNpmScript('test', 'npm-run-all test:*', params);
-            await addNpmScript('test:client', 'mocha test/client --recursive --compilers js:babel-register', params);
-            await addNpmScript('test:server', 'mocha test/server --recursive', params);
+            await addNpmScriptMemory('test', 'npm-run-all test:*', params);
+            await addNpmScriptMemory('test:client', 'mocha test/client --recursive --compilers js:babel-register', params);
+            await addNpmScriptMemory('test:server', 'mocha test/server --recursive', params);
             break;
           case 'angularjs':
-            await addNpmPackage('karma', params, true);
-            await addNpmPackage('karma-chai', params, true);
-            await addNpmPackage('karma-coverage', params, true);
-            await addNpmPackage('karma-mocha', params, true);
+            await addNpmPackageMemory('karma', params, true);
+            await addNpmPackageMemory('karma-chai', params, true);
+            await addNpmPackageMemory('karma-coverage', params, true);
+            await addNpmPackageMemory('karma-mocha', params, true);
             break;
           default:
             break;
         }
-
       } else {
-        await copy(join(__dirname, 'modules', 'mocha', 'app.test.js'), join(build, 'test', 'app.test.js'));
-
-        await addNpmScript('test', 'mocha', params);
+        // Server-side tests
+        set(params, ['build', 'test', 'app.test.js'], await getModule('testing/mocha/app.test.js'));
+        await addNpmScriptMemory('test', 'mocha', params);
       }
       break;
     case 'meteor':
@@ -70,5 +55,3 @@ async function generateTestingMocha(params) {
     default:
   }
 }
-
-export default generateTestingMocha;
