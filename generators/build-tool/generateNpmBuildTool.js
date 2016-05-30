16045144414562a63cfe9d024ae2d78bc476def1
Sahat Yalkabov <sakhat@gmail.com>
2016-05-30 16:00:25 -0400

    Fixed addNpmScript in npm build tool

--- generators/build-tool/generateNpmBuildTool.js
+++ generators/build-tool/generateNpmBuildTool.js
@@ -4,8 +4,8 @@ import { addNpmScriptMemory, addNpmPackageMemory } from '../utils';
 export default async function generateNpmBuildTool(params) {
   await addNpmPackageMemory('npm-run-all', params);
   await addNpmPackageMemory('nodemon', params);
-  await addNpmScript('build', 'npm-run-all build:*', params);
-  await addNpmScript('watch', 'npm-run-all --parallel watch:*', params);
+  await addNpmPackageMemory('build', 'npm-run-all build:*', params);
+  await addNpmPackageMemory('watch', 'npm-run-all --parallel watch:*', params);
 
   switch (params.cssPreprocessor) {
     case 'sass':
