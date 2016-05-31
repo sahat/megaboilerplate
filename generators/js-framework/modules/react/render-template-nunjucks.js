a251d256e30b88c85c555433ae488a6b06f9efa5
Sahat Yalkabov <sakhat@gmail.com>
2016-05-30 21:40:01 -0400

    Properly json stringify initial redux state with nunjucks template engine

--- generators/js-framework/modules/react/render-template-nunjucks.js
+++ generators/js-framework/modules/react/render-template-nunjucks.js
@@ -1,2 +1,2 @@
-var page = nunjucks.render('layout.html', { html: html, initialState: store.getState() });
+var page = nunjucks.render('layout.html', { html: html, initialState: JSON.stringify(store.getState()) });
 res.status(200).send(page);
